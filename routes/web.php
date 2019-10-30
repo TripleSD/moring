e6f51c030207d7597bbd439c5db02c75c9b38613
Anton Zheltyshev <az@maxrival.com>
2019-10-30 18:02:26 +0300

    Added user's block.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::any('/logout','Auth\LoginController@logout')->name('auth.logout');
+Route::any('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 Route::get('/checks/sites', 'ChecksSitesController@getIndex')->name('checks.sites.getIndex');
 
 $groupData = [
@@ -23,8 +23,14 @@ $groupData = [
 ];
 
 Route::group($groupData, function () {
-    $methods =['index', 'create', 'store', 'edit', 'update', 'destroy'];
+    $methods = ['index', 'create', 'store', 'edit', 'update', 'destroy'];
     Route::resource('sites', 'SitesController')
         ->only($methods)
         ->names('admin.sites');
+});
+
+Route::group(['prefix' => 'settings', 'namespace' => 'Admin\Sites','as' => 'settings.'], function () {
+    $methods = ['index', 'create', 'store', 'edit', 'update', 'destroy','show'];
+    Route::resource('users', 'UsersController')
+        ->only($methods);
 });
