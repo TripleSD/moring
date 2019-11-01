78fb98ba9156468e1e1a1396c4634f0581293448
Anton Zheltyshev <az@maxrival.com>
2019-11-01 13:13:30 +0300

    Create file.

--- app/Models/Servers.php
+++ app/Models/Servers.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Servers extends Model
 {
-    //
+    protected $fillable = ['addr', 'description', 'api_token'];
+
+    public function setAddrAttribute($ipAddr)
+    {
+        $this->attributes['addr'] = ip2long($ipAddr);
+    }
 }
