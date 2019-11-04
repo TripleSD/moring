7f9064b02e3c6ab1bf68d9234bd13338ef7371f4
Anton Zheltyshev <az@maxrival.com>
2019-11-04 08:21:34 +0300

    New file.

---
+++ app/Http/Requests/Admin/ServersUpdateRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ServersUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'addr' => 'required|ip',
+            'description' => 'alpha_dash',
+            'enable' => 'nullable|numeric',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'addr.required' => 'Заполните поле IP адреса',
+            'addr.ip' => 'Введите корректо данные в поле IP адреса',
+            'description.alpha_dash' => 'Введите корректно данные в описание сервера',
+        ];
+    }
+}
