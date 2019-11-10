2cd9b4bafcc81b1c7c2b77141c146f9cdbec42bc
Anton Zheltyshev <az@maxrival.com>
2019-11-10 19:22:54 +0300

    Added default value.

--- app/Models/Servers.php
+++ app/Models/Servers.php
@@ -8,6 +8,8 @@ class Servers extends Model
 {
     protected $fillable = ['addr', 'description', 'api_token', 'enable'];
 
+    protected $attributes = ['enabled' => 1];
+
     public function setAddrAttribute($ipAddr)
     {
         $this->attributes['addr'] = ip2long($ipAddr);
