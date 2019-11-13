78fa83e1a67a2dff0816a0a35c95cbb22439aadd
Anton Zheltyshev <az@maxrival.com>
2019-11-13 23:32:59 +0300

    Updated template. Added navbar inside server's page.

--- resources/views/admin/servers/show.blade.php
+++ resources/views/admin/servers/show.blade.php
@@ -36,21 +36,52 @@
                             </span>
                         </div>
 
+
                         <div class="card-body">
-                            <div class="col-sm-6">
-                                <dl>
-                                    <dt> ID сервера:</dt>
-                                    <dd>{{ $server->id }}</dd>
-                                    <dt> IP адрес сервера:</dt>
-                                    <dd>{{ long2ip($server->addr) }}</dd>
-                                    <dt> Краткое описание:</dt>
-                                    <dd>{{ $server->description }}</dd>
-                                </dl>
+                            <div class="row">
+                                <div class="col-5 col-sm-3">
+                                    <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist"
+                                         aria-orientation="vertical">
+                                        <a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill"
+                                           href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home"
+                                           aria-selected="true">Основное</a>
+                                        <a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill"
+                                           href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile"
+                                           aria-selected="false">Агент</a>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-sm-9">
+                                    <div class="tab-content" id="vert-tabs-tabContent">
+                                        <div class="tab-pane text-left fade active show" id="vert-tabs-home"
+                                             role="tabpanel" aria-labelledby="vert-tabs-home-tab">
+                                            <div class="col-sm-6">
+                                                <dl>
+                                                    <dt> ID сервера:</dt>
+                                                    <dd>{{ $server->id }}</dd>
+                                                    <dt> IP адрес сервера:</dt>
+                                                    <dd>{{ long2ip($server->addr) }}</dd>
+                                                    <dt> Краткое описание:</dt>
+                                                    <dd>{{ $server->description }}</dd>
+                                                </dl>
+                                            </div>
+                                        </div>
+                                        <div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel"
+                                             aria-labelledby="vert-tabs-profile-tab">
+                                            <div class="callout callout-info">
+                                                @foreach($settingsFile as $value)
+                                                    {{$value}}<br/>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    </div>
     </div>
 @endsection
