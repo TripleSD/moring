5fbf4ca60b4a4e29c51e0f0fdf179049ba16e00f
Anton Zheltyshev <az@maxrival.com>
2019-11-20 06:30:10 +0300

    Changed time interval.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,10 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
-
-        $schedule->command('SitesChecker')->everyMinute();
+        $schedule->command('SitesChecker')->everyThirtyMinutes();
     }
 
     /**
