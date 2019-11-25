4487d33c55c4ef4a5f1fb8c08949165c9570e7b4
Anton Zheltyshev <az@maxrival.com>
2019-11-26 00:14:31 +0300

    Added controller for bridge identificators.

---
+++ app/Http/Controllers/Admin/Statistics/IdentificatorsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Statistics;
+
+use App\Http\Controllers\Controller;
+use App\Models\Settings;
+
+class IdentificatorsController extends Controller
+{
+    public function getIdentificator()
+    {
+        $identificator = Settings::where('parameter', 'identificator')->first();
+        return $identificator->value;
+    }
+}
