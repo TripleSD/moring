6d1028592f65d640b92746223aa4316497482627
Anton Zheltyshev <az@maxrival.com>
2019-11-27 21:39:36 +0300

    Fix url.

--- resources/views/admin/sites/create.blade.php
+++ resources/views/admin/sites/create.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="col-sm-8">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{route('home')}}">Главная</a></li>
                             <li class="breadcrumb-item active">Добавление сайта</li>
                         </ol>
                     </div>
