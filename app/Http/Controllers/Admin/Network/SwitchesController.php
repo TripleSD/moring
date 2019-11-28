b915360f0bdc2ae967a9911e7c50f57fdc428395
Anton Zheltyshev <az@maxrival.com>
2019-11-28 14:25:44 +0300

    New file.

---
+++ app/Http/Controllers/Admin/Network/SwitchesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Network;
+
+use App\Http\Controllers\Controller;
+
+class SwitchesController extends Controller
+{
+    public function getIndex()
+    {
+        return view('empty');
+    }
+}
