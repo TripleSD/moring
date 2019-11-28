0982d99a9b39cea5a782c63c0b3cc60afd64e6c9
Anton Zheltyshev <az@maxrival.com>
2019-11-28 14:31:33 +0300

    Changed title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Moring</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 
