de8332f93487284f60733e291054c08f15cf7267
Anton Zheltyshev <az@maxrival.com>
2019-12-04 17:34:36 +0300

    New file. Integration Telegram controller.

---
+++ app/Http/Controllers/Admin/Settings/Integrations/TelegramIntegrationsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Settings\Integrations;
+
+use App\Http\Controllers\Admin\Settings\SettingsController;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Settings\Integrations\IntegrationTelegramRequest;
+
+class TelegramIntegrationsController extends Controller
+{
+    public $settingsController;
+
+    public function __construct(SettingsController $settingsController)
+    {
+        $this->middleware('auth');
+        $this->settingsController = $settingsController;
+    }
+
+    public function index()
+    {
+        $status = $this->settingsController->getTelegramStatus();
+        $apiToken = $this->settingsController->getApiKey();
+        $chatId = $this->settingsController->getGroupChatId();
+        return view('admin.settings.integrations.index', compact('apiToken', 'chatId', 'status'));
+    }
+
+    public function update(IntegrationTelegramRequest $request)
+    {
+        $request->validated();
+        $this->settingsController->updateTelegramStatus($request);
+        $this->settingsController->updateApiKey($request);
+        $this->settingsController->updateGroupChatId($request);
+        flash('Данные обновлены')->success();
+        return redirect()->route('settings.integrations.telegram.index');
+    }
+}
