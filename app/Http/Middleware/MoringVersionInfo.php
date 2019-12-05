2e9e9f88fd5b1929fcbceb5251424b1bb20e91b4
Anton Zheltyshev <az@maxrival.com>
2019-12-05 13:38:42 +0300

    Updating phpdoc.

--- app/Http/Middleware/MoringVersionInfo.php
+++ app/Http/Middleware/MoringVersionInfo.php
@@ -13,8 +13,8 @@ class MoringVersionInfo
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
