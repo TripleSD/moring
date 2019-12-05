60ba50bd50f7f4d5f2a2f483a12494ad4994a453
Anton Zheltyshev <az@maxrival.com>
2019-12-05 16:23:20 +0300

    Fix. Moving old functional.

--- app/Console/Commands/BridgeMoringVersionChecker.php
+++ app/Console/Commands/BridgeMoringVersionChecker.php
@@ -2,15 +2,17 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\Admin\Statistics\IdentificatorsController;
+use App\Http\Controllers\Admin\Settings\SettingsController;
 use App\Models\MoringVersions;
-use App\Models\Settings;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
 
 class BridgeMoringVersionChecker extends Command
 {
+
+    public $settingsController;
+
     /**
      * The name and signature of the console command.
      *
@@ -33,31 +35,23 @@ class BridgeMoringVersionChecker extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->settingsController = new SettingsController();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
-        $identificator = new IdentificatorsController();
-
-        $httpClient = new Client();
-        $url = Config::get('moring.bridgeUrl') . Config::get('moring.bridgeCreateIdentificatorUrl'); # Url getting from /config/moring.php
-        $response = $httpClient->request('GET', $url, ['allow_redirects' => false]);
-
-        $settings = new Settings();
-        $settings->parameter = 'identificator';
-        $settings->value = json_decode($response->getBody(), true);
-        $settings->save();
+        $identificator = $this->settingsController->getIdentificator();
 
         # Getting availible Moring versions from bridge
         $httpClient = new Client();
-        $url = Config::get('moring.bridgeUrl') . Config::get('moring.bridgeCurrentMoringVersionUrl'); # Url getting from /config/moring.php
-        $response = $httpClient->request('GET', $url,
-            ['query' => ['identificator' => $identificator->getIdentificator()], 'allow_redirects' => false]);
+        $url = Config::get('moring.bridgeUrl') . Config::get(
+                'moring.bridgeCurrentMoringVersionUrl'
+            ); # Url getting from /config/moring.php
+        $response = $httpClient->request(
+            'GET',
+            $url,
+            ['query' => ['identificator' => $identificator], 'allow_redirects' => false]
+        );
         $versionsBridgeArray = json_decode($response->getBody(), true);
 
         foreach ($versionsBridgeArray as $created_at => $build) {
