aeb1e9a53c237337395349dd9bb9c1ae7845d934
Anton Zheltyshev <az@maxrival.com>
2019-12-07 10:49:39 +0300

    Added macos.

--- app/Console/Ping.php
+++ app/Console/Ping.php
@@ -10,20 +10,26 @@ class Ping
 
     public static function pingTarget(string $host): array
     {
-        if (self::OS !== 'WINNT') {
+        if (self::OS == 'WINNT') {
+            exec("ping -c 3 $host", $output, $return_var);
+        } elseif (self::OS == 'Darwin') {
             exec("ping -c 3 $host", $output, $return_var);
         } else {
             exec("ping -n 3 $host", $output, $return_var);
         }
         if (count($output) > 3) {
-            $filter =[];
-            $pre_final = array_reduce($output, function ($filter, $string){
-                preg_match("/[\d | \d.\d]{1,9}(?=ms| ms| TTL|\D{1,3}TTL)/", $string, $matches);
-                if (!empty($matches) && count($filter) < 3) {
-                    $filter[] = floatval($matches[0]);
-                }
-                return $filter;
-            }, $filter);
+            $filter = [];
+            $pre_final = array_reduce(
+                $output,
+                function ($filter, $string) {
+                    preg_match("/[\d | \d.\d]{1,9}(?=ms| ms| TTL|\D{1,3}TTL)/", $string, $matches);
+                    if (!empty($matches) && count($filter) < 3) {
+                        $filter[] = floatval($matches[0]);
+                    }
+                    return $filter;
+                },
+                $filter
+            );
         } else {
             for ($i = 0; $i < 2; ++$i) {
                 $pre_final[$i] = 0;
@@ -31,13 +37,17 @@ class Ping
         }
 
         $keys = ['first', 'second', 'third'];
-        array_map(function ($key, $value) use (&$final){
-            if ($value !== null) {
-                $final[$key] = $value;
-            } else {
-                $final[$key] = 0;
-            }
-        }, $keys, $pre_final);
+        array_map(
+            function ($key, $value) use (&$final) {
+                if ($value !== null) {
+                    $final[$key] = $value;
+                } else {
+                    $final[$key] = 0;
+                }
+            },
+            $keys,
+            $pre_final
+        );
         return $final;
     }
 }
