b1f95972490c4411e72c55e48c1e1a20be269efc
Anton Zheltyshev <az@maxrival.com>
2019-12-08 22:27:17 +0300

    Added updating timestamp function.

--- app/Console/Commands/BridgeMoringVersionChecker.php
+++ app/Console/Commands/BridgeMoringVersionChecker.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Http\Controllers\Admin\Settings\SettingsController;
 use App\Models\MoringVersions;
+use App\Repositories\BridgeRepository;
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
@@ -11,7 +12,8 @@ use Illuminate\Support\Facades\Config;
 class BridgeMoringVersionChecker extends Command
 {
 
-    public $settingsController;
+    private $settingsController;
+    private $bridgeRepository;
 
     /**
      * The name and signature of the console command.
@@ -36,6 +38,7 @@ class BridgeMoringVersionChecker extends Command
     {
         parent::__construct();
         $this->settingsController = new SettingsController();
+        $this->bridgeRepository = new BridgeRepository();
     }
 
     public function handle()
@@ -67,5 +70,6 @@ class BridgeMoringVersionChecker extends Command
                 echo $e->getMessage();
             }
         }
+        $this->bridgeRepository->updateStatInfo('bridge_moring_versions');
     }
 }
