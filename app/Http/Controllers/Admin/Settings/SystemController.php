f6a17b6554b89db7a480fdca70b7c41b6534055c
Anton Zheltyshev <az@maxrival.com>
2019-12-08 22:27:49 +0300

    Added bridge stat inforamtion.

--- app/Http/Controllers/Admin/Settings/SystemController.php
+++ app/Http/Controllers/Admin/Settings/SystemController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers\Admin\Settings;
 
 use App\Http\Controllers\Controller;
 use App\Models\Users;
+use App\Repositories\BridgeRepository;
 use Hash;
 use Illuminate\Http\Request;
 
 class SystemController extends Controller
 {
 
+    private $bridgeRepository;
+
     public function __construct()
     {
         $this->middleware('auth');
+        $this->bridgeRepository = new BridgeRepository();
     }
 
     public function index()
     {
-//        shell_exec('cat /proc/cpuinfo');
-//        $os = php_uname();
-//        return view('admin.settings.system.index', compact('os'));
-
-        return view('empty');
+        $bridgeStatistics = $this->bridgeRepository->getBridgeStatistics();
+        $os = php_uname();
+        return view('admin.settings.system.index', compact('os','bridgeStatistics'));
     }
 }
