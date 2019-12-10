f1c38116c948414a7ac9b8b81c0accded31dd34c
Anton Zheltyshev <az@maxrival.com>
2019-12-10 16:45:19 +0300

    Fix template.

--- resources/views/admin/settings/system/index.blade.php
+++ resources/views/admin/settings/system/index.blade.php
@@ -5,14 +5,13 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    {{--                    <h1 class="m-0 text-dark">{{ $server->description }} ({{long2ip($server->addr)}})</h1>--}}
+                    <h1 class="m-0 text-dark">Настройки</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{route('home')}}">Главная</a></li>
                         <li class="breadcrumb-item">Мониторинг</li>
-                        <li class="breadcrumb-item"><a href="{{route('servers.index')}}">Серверы</a></li>
-                        <li class="breadcrumb-item active">Карточка сервера</li>
+                        <li class="breadcrumb-item active">Система</li>
                     </ol>
                 </div>
             </div>
@@ -25,19 +24,14 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Карточка сервера</h3>
-                            <span class="float-right">
-                                <a href="{{route('servers.index')}}"
-                                   class="btn btn-sm bg-gradient-info" title="Вернуться">
-                                    <i class="fa fa-arrow-left"></i></a>
-                            </span>
+                            <h3 class="card-title">Система</h3>
                         </div>
 
                         <div class="card-body">
                             <dt> Операционная система:</dt>
                             <dd>{{ php_uname() }}</dd>
                             <dt> IP адрес сервера:</dt>
-                            <dd>{{ PHP_OS }}</dd>
+                            <dd>{{ request()->server('SERVER_ADDR') }}</dd>
                             <dt> Версия PHP интерпретатора:</dt>
                             <dd>{{ phpversion()  }}</dd>
                             <dt>Временная зона</dt>
