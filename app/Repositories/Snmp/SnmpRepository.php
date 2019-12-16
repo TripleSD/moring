b9bcde4d6b7f73a750d8ed97b5f555ff2d2097d9
Anton Zheltyshev <az@maxrival.com>
2019-12-16 15:41:53 +0300

    Added snmp repository.

---
+++ app/Repositories/Snmp/SnmpRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repositories\Snmp;
+
+use App\Repositories\Repository;
+
+class SnmpRepository extends Repository
+{
+    public function getInfo($community, $version, $ip, $oid)
+    {
+        return shell_exec("/usr/bin/snmpwalk -$version -c$community $ip $oid");
+    }
+
+}
