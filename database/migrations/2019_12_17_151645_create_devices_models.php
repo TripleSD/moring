f3defccc6c3de99a2d4d0d277f12f2efac94734d
Anton Zheltyshev <az@maxrival.com>
2019-12-17 15:26:28 +0300

    New migration. Devices models.

---
+++ database/migrations/2019_12_17_151645_create_devices_models.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesModels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices_models', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('device_id');
+            $table->string('title')->comment('Description for view');
+            $table->foreign('device_id')
+                ->references('id')->on('network_devices');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices_models');
+    }
+}
