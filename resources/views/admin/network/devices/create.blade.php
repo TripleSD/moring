086d51593b7e3085845c64151981d77116f3d4bb
Anton Zheltyshev <az@maxrival.com>
2019-12-19 12:17:20 +0300

    New file.

--- resources/views/admin/network/devices/create.blade.php
+++ resources/views/admin/network/devices/create.blade.php
@@ -4,58 +4,82 @@
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Мониторинг</h1>
+                <div class="col-sm-4">
+                    <h1 class="m-0 text-dark">Сетевые устройства</h1>
                 </div>
-                <div class="col-sm-6">
+                <div class="col-sm-8">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{route('home')}}">Главная</a></li>
-                        <li class="breadcrumb-item">Мониторинг</li>
-                        <li class="breadcrumb-item active">Серверы</li>
+                        <li class="breadcrumb-item">Сеть</li>
+                        <li class="breadcrumb-item active">Сетевые устройства</li>
                     </ol>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Добавление нового сервера</h3>
-                            <div class="card-tools">
-                                <a href="{{route('servers.index')}}"
-                                   class="btn btn-sm bg-gradient-info" title="Вернуться">
-                                    <i class="fa fa-arrow-left"></i></a>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Добавление нового устройства</h3>
+                                <div class="card-tools">
+                                    <a href="{{route('network.devices.index')}}"
+                                       class="btn btn-sm bg-gradient-info" title="Вернуться">
+                                        <i class="fa fa-arrow-left"></i></a>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="card-body">
-                            <div class="col-sm-6">
-                                {{ Form::open(['url' => route('servers.store'),'method' => 'post', 'enctype' => "multipart/form-data"]) }}
+                            <div class="card-body">
+                                <div class="col-sm-6">
+                                    {{ Form::open([ 'route' => 'network.devices.store', 'method' => 'post', 'enctype' => "multipart/form-data"]) }}
+                                    <div class="form-group">
+                                        <b>Сетевое имя устройства или IP адрес</b>
+                                        {{ Form::text('hostname', null , ['class' => 'form-control', 'required', 'placeholder' => 'mydevice.local или 192.168.88.1']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
 
-                                <div class="form-group">
-                                    <label>IP адрес сервера
-                                        <span class="text text-danger text-sm">*</span></label>
-                                    {{ Form::text('addr', null , ['class' => 'form-control', 'required','placeholder' => '127.0.0.1']) }}
+                                    <div class="form-group">
+                                        <b>Версия SNMP протокола</b>
+                                        {{ Form::text('snmp_version', '2' , ['class' => 'form-control', 'required', 'placeholder' => 'Пример: 2']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Порт SNMP</b>
+                                        {{ Form::text('snmp_port', '161', ['class' => 'form-control', 'required', 'placeholder' => 'Пример: 161']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>SNMP community</b>
+                                        {{Form::text('community', 'public', ['class' => 'form-control', 'required', 'placeholder' => 'Пример: public'])}}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+                                    <button type="submit" class="btn btn-xs bg-gradient-cyan">Добавить</button>
+                                    {{ Form::close() }}
                                 </div>
-
-                                <div class="form-group">
-                                    <label>Краткое описание</label>
-                                    {{ Form::text('description', null , ['class' => 'form-control', 'placeholder' => 'Тестовый сервер']) }}
-                                </div>
-
-                                <div class="form-group">
-                                    <label>API Token</label>
-                                    {{ Form::text('api_token', $token , ['class' => 'form-control', 'required', 'disabled']) }}
-                                    {{ Form::text('api_token', $token , ['class' => 'form-control', 'required', 'hidden']) }}
-                                </div>
-
-                                <button type="submit" class="btn btn-sm bg-gradient-success">Добавить</button>
-                                {{ Form::close() }}
-                                <div class="text text-danger text-xs float-right">* (обязательный параметр)</div>
                             </div>
                         </div>
                     </div>
--- resources/views/admin/network/devices/edit.blade.php
+++ resources/views/admin/network/devices/edit.blade.php
@@ -1,61 +1,179 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Настройки</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Главная</a></li>
-                        <li class="breadcrumb-item">Настройки</li>
-                        <li class="breadcrumb-item active"><a href="{{ route('settings.users.index') }}">
-                                Пользователи</a></li>
-                    </ol>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-4">
+                        <h1 class="m-0 text-dark">Редактирование сайта</h1>
+                    </div>
+                    <div class="col-sm-8">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Редактирование сайта</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Редактирование карточки пользователя</h3>
-                            <span class="float-right">
-                                <a href="{{route('servers.show',$server->id)}}"
-                                   class="btn btn-sm bg-gradient-info" title="Вернуться">
-                                                    <i class="fa fa-arrow-left"></i>
-                                                </a>
-                            </span>
-                        </div>
-
-                        <div class="card-body">
-                            <div class="col-sm-6">
-                                {{ Form::open(['url' => route('servers.update', $server->id),'method' => 'post', 'enctype' => "multipart/form-data"]) }}
-                                @method('patch')
-
-                                <div class="form-group">
-                                    <label>IP адрес</label>
-                                    {{ Form::text('addr', long2ip($server->addr) , ['class' => 'form-control', 'required','placeholder' => '192.168.0.1']) }}
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Редактирование сайта</h3>
+                                <div class="card-tools">
+                                    <a href="{{route('admin.sites.show', $site->id)}}"
+                                       class="btn btn-sm bg-gradient-info" title="Вернуться">
+                                        <i class="fa fa-arrow-left"></i></a>
                                 </div>
+                            </div>
 
-                                <div class="form-group">
-                                    <label>Краткое описание</label>
-                                    {{ Form::text('description', $server->description, ['class' => 'form-control', 'placeholder' => 'dev сервер']) }}
+                            <div class="card-body">
+                                <div class="col-sm-6">
+                                    {{ Form::open([ 'url' => route('admin.sites.update', $site->id), 'method' => 'put', 'enctype' => "multipart/form-data"]) }}
+
+                                    <div class="form-group">
+                                        <label>Название сайта</label>
+                                        {{ Form::text('title', $site->title , ['class' => 'form-control', 'required','placeholder' => 'My website or so']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Введите название сайта, которое поможет Вам легче ориентироваться в списке сайтов.
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Адрес URL</label>
+                                        {{ Form::text('url', $site->url , ['class' => 'form-control', 'required', 'placeholder' => 'yourdomain.com/']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Введите URL сайта без указания http/https префиксов - эти настройки будут дальше.
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Мониторить сайт</label><br>
+                                        <div class="form-check-inline">
+                                            {{Form::text('enabled', '0', ['class' => 'form-control', 'hidden'])}}
+                                            {{ Form::checkbox('enabled', true, $site->enabled) }}
+                                        </div>
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Для активации мониторинга отметьте данный чекбокс.
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Путь к файлу мониторинга</label>
+                                        {{ Form::text('file_url', $site->file_url , ['class' => 'form-control', 'placeholder' => 'monitoring.php']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Вы можете получать более подробную информацию о состоянии Вашего оборудования.
+                                            Для этого необходимо установить файл мониторинга на контролируемый сервер (необходимо вводить полный путь к файлу)
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Использовать файл мониторинга</label><br>
+                                        <div class="form-check-inline">
+                                            {{Form::text('use_file', '0', ['class' => 'form-control', 'hidden'])}}
+                                            {{ Form::checkbox('use_file', true, $site->checksList->use_file) }}
+                                        </div>
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Если Вы не желаете использовать файл мониторинг на текущем этапе, просто снимите отметку с данного чекбокса.
+                                        </details>
+                                    </div>
+                                    <div class="form-group">
+                                        <div>
+                                            <b>HTTP ответ сервера</b>
+                                            {{Form::text('http_code', '0', ['class' => 'form-control', 'hidden'])}}
+                                            {{ Form::checkbox('http_code', 1, true) }}</div>
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Проверка возвращаемого сайтом ответа на HTTP запрос.
+                                        </details>
+                                    </div>
+
+                                    <div class="form-row">
+                                        <div class="form-group col-md-3">
+                                            <label>Поддержка HTTPS</label><br>
+                                            <div class="form-check-inline">
+                                            {{Form::text('https', '0', ['class' => 'form-control', 'hidden'])}}
+                                                {{ Form::checkbox('https', true, $site->https) }}
+                                            </div>
+                                            <details class="mt--3 small">
+                                                <summary>
+                                                    Дополнительная информация
+                                                </summary>
+                                                Если сервер поддерживает защищенное соединение (HTTPS), отметьте этот чекбокс для получения корректных данных об ответе сервера на HTTP запрос.
+                                            </details>
+                                        </div>
+                                        <div class="form-group col-md-4">
+                                            <label>Мониторинг SSL</label><br>
+                                            <div class="form-check-inline">
+                                            {{Form::text('check_ssl', '0', ['class' => 'form-control', 'hidden'])}}
+                                                {{ Form::checkbox('check_ssl', true, $site->checksList->check_ssl) }}
+                                            </div>
+                                            <details class="mt--3 small">
+                                                <summary>
+                                                    Дополнительная информация
+                                                </summary>
+                                                Отметьте чекбокс для проверки SSL сертификата сервера.
+                                            </details>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Контроль версии PHP</label><br>
+                                        <div class="form-check-inline">
+                                            {{Form::text('check_php', '0', ['class' => 'form-control', 'hidden'])}}
+                                            {{ Form::checkbox('check_php', true, $site->checksList->check_php) }}
+                                        </div>
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            Отметьте чекбокс для контроля версии PHP.
+                                        </details>
+
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label>Описание</label>
+                                        {{ Form::text('comment', $site->comment, ['class' => 'form-control', 'placeholder' => 'Это мой любимый сайт и я всегда радуюсь, видя его онлайн, но иногда...']) }}
+                                        <summary>
+                                            Дополнительная информация
+                                        </summary>
+                                        Здесь Вы можете ввести дополнительную информацию для болеее полного описания сайта.
+                                        </details>
+                                    </div>
+                                    <div class="form-row ">
+                                        <div class="form-group col-md-4">
+                                    <button type="submit" class="btn btn-xs bg-gradient-cyan">Обновить</button>
+                                    {{ Form::close() }}
+                                        </div>
+                                        <div class="form-group col-md-4">
+                                    {{Form::open([ 'url' => route('admin.sites.destroy', $site->id), 'method' => 'delete', 'enctype' => "multipart/form-data"])}}
+                                    <button type="submit" class="btn btn-xs bg-gradient-red">Удалить</button>
+                                    {{ Form::close() }}
+                                        </div>
+                                    </div>
                                 </div>
-
-                                <div class="form-group">
-                                    <label>Включен/Не включен:</label>
-                                    {{ Form::checkbox('enabled', true, $server->enabled) }}
-                                </div>
-
-                                <button type="submit" class="btn btn-sm bg-gradient-success">Сохранить</button>
-                                {{ Form::close() }}
                             </div>
                         </div>
                     </div>
