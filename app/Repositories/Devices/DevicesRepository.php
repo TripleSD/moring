e074992bac56b53f6e4b4d06aa579a5279cd1363
Anton Zheltyshev <az@maxrival.com>
2019-12-19 12:16:57 +0300

    New file.

---
+++ app/Repositories/Devices/DevicesRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories\Devices;
+
+use App\Models\Devices;
+use App\Repositories\Repository;
+
+class DevicesRepository extends Repository
+{
+    public function saveDevice(
+        $hostname,
+        $vendorId,
+        $modelId,
+        $firmwareId,
+        $uptimeDevice,
+        $contact,
+        $location,
+        $humanModel,
+        $licenseLevel,
+        $serialNumber,
+        $packetsVersion
+    ) {
+        $device = new Devices();
+        $device->hostname = $hostname;
+        $device->vendor_id = $vendorId;
+        $device->model_id = $modelId;
+        $device->firmware_id = $firmwareId;
+        $device->uptime = $uptimeDevice;
+        $device->contact = $contact;
+        $device->location = $location;
+        $device->human_model = $humanModel;
+        $device->license_level = $licenseLevel;
+        $device->serial_number = $serialNumber;
+        $device->packets_version = $packetsVersion;
+        $device->save();
+        return true;
+    }
+}
