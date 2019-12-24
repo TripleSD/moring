fca490d12f955fcd0102bbdff328dd169878b70a
Anton Zheltyshev <az@maxrival.com>
2019-12-24 21:27:34 +0300

    Fix code style.

--- app/Repositories/Snmp/Vendors/Cisco.php
+++ app/Repositories/Snmp/Vendors/Cisco.php
@@ -20,7 +20,7 @@ class Cisco extends Repository
         $string = str_replace('STRING: ', '', $string);
         $string = str_replace('"', '', $string);
 
-        return (string) trim($string);
+        return (string)trim($string);
     }
 
     public function getFirmware($snmpFlow): string
@@ -28,7 +28,7 @@ class Cisco extends Repository
         $string = $snmpFlow->get('SNMPv2-MIB::sysDescr.0');
         preg_match('/Cisco IOS Software/', $string, $match);
 
-        return (string) $match[0];
+        return (string)$match[0];
     }
 
     public function getFirmwareVersion($snmpFlow): ?string
@@ -38,63 +38,92 @@ class Cisco extends Repository
             $string = str_replace('STRING: ', '', $string);
             $string = str_replace('"', '', $string);
 
-            return (string) trim($string);
+            return (string)trim($string);
         } catch (\Exception $e) {
             return null;
         }
     }
 
+    /**
+     * @param $snmpFlow
+     */
     public function getPacketsVersion($snmpFlow): void
     {
     }
 
+    /**
+     * @param $snmpFlow
+     * @return string
+     */
     public function getUptime($snmpFlow): string
     {
         $string = $snmpFlow->get('.1.3.6.1.2.1.1.3.0');
         preg_match('/\((\d+)\)/', $string, $string);
         preg_match('/\d+/', $string[1], $string);
 
-        return (string) trim($string[0]);
+        return (string)trim($string[0]);
     }
 
+    /**
+     * @param $snmpFlow
+     * @return string
+     */
     public function getContact($snmpFlow): string
     {
         $string = $snmpFlow->get('SNMPv2-MIB::sysContact.0');
         $string = str_replace('STRING: ', '', $string);
 
-        return (string) trim($string);
+        return (string)trim($string);
     }
 
+    /**
+     * @param $snmpFlow
+     * @return string
+     */
     public function getLocation($snmpFlow): string
     {
         $string = $snmpFlow->get('SNMPv2-MIB::sysLocation.0');
         $string = str_replace('STRING: ', '', $string);
 
-        return (string) trim($string);
+        return (string)trim($string);
     }
 
-    public function getSerialnNumber($snmpFlow): ?string
+    /**
+     * @param $snmpFlow
+     * @return string|null
+     */
+    public function getSerialNumber($snmpFlow): ?string
     {
         try {
             $string = $snmpFlow->get('.1.3.6.1.2.1.47.1.1.1.1.11.1001');
             $string = str_replace('STRING: ', '', $string);
 
-            return (string) trim($string);
+            return (string)trim($string);
         } catch (\Exception $e) {
             return null;
         }
     }
 
+    /**
+     * @param $snmpFlow
+     */
     public function getHumanModel($snmpFlow): void
     {
     }
 
+    /**
+     * @param $snmpFlow
+     */
     public function getLicenseLevel($snmpFlow): void
     {
     }
 
-    public function getPlarformType($model): int
+    /**
+     * @param $model
+     * @return int
+     */
+    public function getPlatformType($model): int
     {
-        return (int) 0;
+        return (int)0;
     }
 }
