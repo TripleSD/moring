2f3a5c119639bfd422d904adebcb51189b8614b0
Anton Zheltyshev <az@maxrival.com>
2019-12-26 17:22:22 +0300

    Code style fix & added exception.

--- app/Repositories/Snmp/SnmpRepository.php
+++ app/Repositories/Snmp/SnmpRepository.php
@@ -2,38 +2,53 @@
 
 namespace App\Repositories\Snmp;
 
+use Exception;
 use App\Repositories\Repository;
 use SNMP;
 
 class SnmpRepository extends Repository
 {
+    /**
+     * @param $host
+     * @param $community
+     * @return SNMP
+     */
     public function getSnmpFlow($host, $community)
     {
         return new SNMP(SNMP::VERSION_2c, $host, $community);
     }
 
+    /**
+     * @param $snmpFlow
+     * @return string
+     * @throws Exception
+     */
     public function getVendor($snmpFlow)
     {
         $vendor = (string) null;
 
-        $string = $snmpFlow->get('1.3.6.1.2.1.1.1.0');
+        try {
+            $string = $snmpFlow->get('1.3.6.1.2.1.1.1.0');
 
-        if (preg_match('/RouterOS/', $string)) {
-            $vendor = 'MikroTik';
-        }
+            if (preg_match('/RouterOS/', $string)) {
+                $vendor = 'MikroTik';
+            }
 
-        if (preg_match('/Cisco/', $string)) {
-            $vendor = 'Cisco';
-        }
+            if (preg_match('/Cisco/', $string)) {
+                $vendor = 'Cisco';
+            }
 
-        if (preg_match('/DGS-/', $string)) {
-            $vendor = 'D-Link';
-        } elseif (preg_match('/DES-/', $string)) {
-            $vendor = 'D-Link';
-        }
+            if (preg_match('/DGS-/', $string)) {
+                $vendor = 'D-Link';
+            } elseif (preg_match('/DES-/', $string)) {
+                $vendor = 'D-Link';
+            }
 
-        if (preg_match('/MES/', $string)) {
-            $vendor = 'Eltex';
+            if (preg_match('/MES/', $string)) {
+                $vendor = 'Eltex';
+            }
+        } catch (\Exception $e) {
+            throw new Exception('SNMP поток получен, но определить производителя не удалось.');
         }
 
         return (string) $vendor;
