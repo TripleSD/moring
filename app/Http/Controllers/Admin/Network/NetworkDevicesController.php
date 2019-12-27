a36c8a2bc5d8ab8e6b609a9193347d6d0b5eea09
Anton Zheltyshev <az@maxrival.com>
2019-12-27 21:50:04 +0300

    Added try/catch & moved some methods.

--- app/Http/Controllers/Admin/Network/NetworkDevicesController.php
+++ app/Http/Controllers/Admin/Network/NetworkDevicesController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers\Admin\Network;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Devices\DevicesVendorsRepository;
 use App\Http\Requests\Sites\UpdateAndStoreDeviceRequest;
-use App\Models\Devices;
-use App\Models\DevicesFirmwares;
-use App\Models\DevicesModels;
-use App\Models\DevicesVendors;
 use App\Repositories\Devices\DevicesRepository;
-use App\Repositories\Snmp\SnmpRepository;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -17,24 +13,26 @@ use Illuminate\View\View;
 
 class NetworkDevicesController extends Controller
 {
-    /** @var SnmpRepository */
-    private $snmpRepository;
-
     /** @var DevicesRepository */
     private $deviceRepository;
+    private $deviceVendorsRepository;
 
+    /**
+     * NetworkDevicesController constructor.
+     */
     public function __construct()
     {
-        $this->snmpRepository   = new SnmpRepository();
-        $this->deviceRepository = new DevicesRepository();
+        $this->deviceRepository        = new DevicesRepository();
+        $this->deviceVendorsRepository = new DevicesVendorsRepository();
     }
 
     /**
+     * @param Request $request
      * @return Factory|View
      */
-    public function index()
+    public function index(Request $request)
     {
-        $devices = Devices::with('firmware', 'model', 'vendor')->get();
+        $devices = $this->deviceRepository->getDevicesList($request);
 
         return view('admin.network.devices.index', compact('devices'));
     }
@@ -47,9 +45,13 @@ class NetworkDevicesController extends Controller
         return view('admin.network.devices.create');
     }
 
+    /**
+     * @param Request $request
+     * @return Factory|View
+     */
     public function edit(Request $request)
     {
-        $device = Devices::with('model', 'vendor', 'firmware')->find($request->device);
+        $device = $this->deviceRepository->editDevice($request->device);
 
         return view('admin.network.devices.edit', compact('device'));
     }
@@ -60,167 +62,50 @@ class NetworkDevicesController extends Controller
      */
     public function destroy(Request $request)
     {
-        $device = Devices::find($request->device);
-        $device->delete();
-
-        flash('Устройство удалено')->success();
+        $this->deviceRepository->destroyDevice($request->device);
 
         return redirect()->route('network.devices.index');
     }
 
+    /**
+     * @param Request $request
+     * @return Factory|View
+     */
     public function show(Request $request)
     {
-        $device = Devices::with('model', 'vendor', 'firmware')->find($request->device);
+        $device = $this->deviceRepository->show($request->device);
 
         return view('admin.network.devices.show', compact('device'));
     }
 
+
+    /**
+     * @param UpdateAndStoreDeviceRequest $request
+     * @return RedirectResponse
+     */
     public function update(UpdateAndStoreDeviceRequest $request)
     {
-        $fill   = $request->validated();
-        $device = Devices::find($request->device);
-        $device->update($fill);
+        $this->deviceRepository->updateDevice($request);
         flash('Данные успешно обнолены')->success();
 
         return redirect()->route('network.devices.show', $request->device);
     }
 
+
     /**
      * @param Request $request
      * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         try {
-            // Getting vars from template
-            $hostname      = $request->input('hostname');               // Hostname device
-            $title         = $request->input('title');                  // Short description
-            $snmpCommunity = $request->input('snmp_community');         // Device community
-            $snmpPort      = $request->input('snmp_port');              // Device snmp port
-            $snmpVersion   = $request->input('snmp_version');           // Device snmp version 1/2/3
-
-            // Getting snmp flow from device
-            $snmpFlow = $this->snmpRepository->getSnmpFlow(
-                $hostname,
-                $snmpCommunity
-            );
-
-            // Identification vendor
-            $vendor      = $this->snmpRepository->getVendor($snmpFlow);
-            $vendorClass = str_replace('-', '', $vendor); // Replace unused symbols
-
-            // Checking vendor isset on system
-            if ($vendor == null) {
-                flash('Производитель не определен, либо устройства данного производителя не поддерживается')
-                    ->warning();
-
-                return redirect()->back()->withInput();
-            }
-
-            $firmwareClassFile = '\App\Repositories\Snmp\Vendors\\' . $vendorClass;
-            $firmwareClass     = new $firmwareClassFile();
-
-            // Getting vars from device
-            $location        = $firmwareClass->getLocation($snmpFlow);
-            $contact         = $firmwareClass->getContact($snmpFlow);
-            $model           = $firmwareClass->getModel($snmpFlow);
-            $platformType    = $firmwareClass->getPlatformType($model);
-            $firmwareTitle   = $firmwareClass->getFirmware($snmpFlow);
-            $firmwareVersion = $firmwareClass->getFirmwareVersion($snmpFlow);
-            $uptimeDevice    = $firmwareClass->getUptime($snmpFlow);
-            $packetsVersion  = $firmwareClass->getPacketsVersion($snmpFlow);
-            $serialNumber    = $firmwareClass->getSerialNumber($snmpFlow);
-            $humanModel      = $firmwareClass->getHumanModel($snmpFlow);
-            $licenseLevel    = $firmwareClass->getLicenseLevel($snmpFlow);
-
-            $firmware = $this->checkFirmware($firmwareTitle, $firmwareVersion);
-            $vendorId = $this->checkVendor($vendor);
-            $modelId  = $this->checkModel($model);
-            $this->deviceRepository->saveDevice(
-                $title,
-                $hostname,
-                $vendorId,
-                $modelId,
-                $firmware,
-                $uptimeDevice,
-                $contact,
-                $location,
-                $humanModel,
-                $licenseLevel,
-                $serialNumber,
-                $packetsVersion,
-                $platformType,
-                $snmpPort,
-                $snmpCommunity,
-                $snmpVersion
-            );
-            flash('Хост добавлен')->success();
+            $this->deviceRepository->getDeviceData($request);
 
             return redirect()->route('network.devices.index');
-        } catch (\Exception $e) {
-            flash('Возникла ошибка при добавлении нового устройства! (' . $e->getMessage() . ')')->warning();
+        } catch (\Exception $exception) {
+            flash($exception->getMessage())->warning();
 
             return redirect()->back()->withInput();
-        }
-    }
-
-    /**
-     * @param string $vendorTitle
-     * @return int
-     */
-    public function checkVendor(string $vendorTitle): int
-    {
-        $vendor = DevicesVendors::where('title', $vendorTitle)->first();
-        if (empty($vendor)) {
-            $vendor        = new DevicesVendors();
-            $vendor->title = $vendorTitle;
-            $vendor->save();
-
-            return $vendor->id;
-        } else {
-            return $vendor->id;
-        }
-    }
-
-    /**
-     * @param string $modelTitle
-     * @return int
-     */
-    public function checkModel(string $modelTitle): int
-    {
-        $model = DevicesModels::where('title', $modelTitle)->first();
-
-        if (empty($model)) {
-            $model        = new DevicesModels();
-            $model->title = $modelTitle;
-            $model->save();
-
-            return $model->id;
-        } else {
-            return $model->id;
-        }
-    }
-
-    /**
-     * @param string $firmwareTitle
-     * @param string $firmwareVersion
-     * @return int
-     */
-    public function checkFirmware(string $firmwareTitle, string $firmwareVersion = null): int
-    {
-        $firmware = DevicesFirmwares::where('title', $firmwareTitle)
-            ->where('version', $firmwareVersion)
-            ->first();
-
-        if (empty($firmware)) {
-            $firmware          = new DevicesFirmwares();
-            $firmware->title   = $firmwareTitle;
-            $firmware->version = $firmwareVersion;
-            $firmware->save();
-
-            return $firmware->id;
-        } else {
-            return $firmware->id;
         }
     }
 }
