1656ad2c23ed76a19b7e7ead8b58ad499d594a19
Anton Zheltyshev <az@maxrival.com>
2019-12-27 22:16:29 +0300

    Refactoring & added exception.

--- app/Repositories/Snmp/SnmpRepository.php
+++ app/Repositories/Snmp/SnmpRepository.php
@@ -18,37 +18,32 @@ class SnmpRepository extends Repository
         return new SNMP(SNMP::VERSION_2c, $host, $community);
     }
 
-    /**
-     * @param $snmpFlow
-     * @return string
-     * @throws Exception
-     */
-    public function getVendor($snmpFlow)
+    public function getVendor($snmpFlow): string
     {
         $vendor = (string) null;
 
         try {
             $string = $snmpFlow->get('1.3.6.1.2.1.1.1.0');
+        } catch (Exception $exception) {
+            throw new Exception('MIB не содержит данных о производителе.');
+        }
 
-            if (preg_match('/RouterOS/', $string)) {
-                $vendor = 'MikroTik';
-            }
+        if (preg_match('/RouterOS/', $string)) {
+            $vendor = 'MikroTik';
+        }
 
-            if (preg_match('/Cisco/', $string)) {
-                $vendor = 'Cisco';
-            }
+        if (preg_match('/Cisco/', $string)) {
+            $vendor = 'Cisco';
+        }
 
-            if (preg_match('/DGS-/', $string)) {
-                $vendor = 'D-Link';
-            } elseif (preg_match('/DES-/', $string)) {
-                $vendor = 'D-Link';
-            }
+        if (preg_match('/DGS-/', $string)) {
+            $vendor = 'D-Link';
+        } elseif (preg_match('/DES-/', $string)) {
+            $vendor = 'D-Link';
+        }
 
-            if (preg_match('/MES/', $string)) {
-                $vendor = 'Eltex';
-            }
-        } catch (\Exception $e) {
-            throw new Exception('SNMP поток получен, но определить производителя не удалось.');
+        if (preg_match('/MES/', $string)) {
+            $vendor = 'Eltex';
         }
 
         return (string) $vendor;
