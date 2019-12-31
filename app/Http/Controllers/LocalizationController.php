de1c1f99c74788bc5969fc0402618ac9b592a837
Anton Zheltyshev <az@maxrival.com>
2019-12-31 22:32:15 +0300

    Removed unused code.

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -8,7 +8,6 @@ class LocalizationController extends Controller
 {
     public function index($locale)
     {
-        App::setLocale($locale);
         session()->put('locale', $locale);
         return redirect()->back();
     }
