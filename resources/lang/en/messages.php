2ff176040b29e79783e3f79c70275811c476a03c
Anton Zheltyshev <az@maxrival.com>
2019-12-31 21:11:12 +0300

    Added dictionary.

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    /**
+     * Top menu
+     */
+    'top_menu.exit' => 'Exit',
+    'top_menu.current_version' => 'You have current version',
+    'top_menu.new_version' => 'Available new version',
+    'top_menu.from' => 'from',
+
+    /**
+     *  Главное меню
+     */
+    'main_menu.home' => 'Home',
+    'main_menu.news' => 'News',
+    'main_menu.network' => 'Network',
+    'main_menu.network.devices' => 'Devices',
+
+    'main_menu.infrastructure' => 'Infrastructure',
+    'main_menu.infrastructure.servers' => 'Servers',
+    'main_menu.hosting' => 'Hosting',
+    'main_menu.hosting.sites' => 'Sites',
+
+    'main_menu.settings' => 'Settings',
+    'main_menu.settings.users' => 'Users',
+    'main_menu.settings.system' => 'System',
+    'main_menu.settings.bridge' => 'Bridge',
+    'main_menu.settings.integrations' => 'Integrations',
+
+    'main_menu.help' => 'Help',
+    'main_menu.help.documentation' => 'Documentation',
+    'main_menu.help.changelog' => 'Changelog',
+    'main_menu.contacts' => 'Contacts',
+
+    /**
+     *  Breadcrumbs menu
+     */
+    'breadcrumbs.main' => 'Main',
+    'breadcrumbs.network' => 'Network',
+
+    /**
+     * Device card
+     */
+    'device.notifications_and_errors' => 'Notifications & errors',
+    'device.errors' => 'Errors',
+    'device.snmp_fail' => 'SNMP. Error connection to device',
+    'device.model' => 'Model',
+    'device.firmware_version' => 'Firmware version',
+    'device.date_time' => 'Date / time',
+    'device.platform_type' => 'Platform type',
+    'device.port' => 'port',
+    'device.ports' => 'Ports',
+];
---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    /**
+     *  Верхнее меню
+     */
+    'top_menu.exit' => 'Выход',
+    'top_menu.current_version' => 'Установлена последняя версия',
+    'top_menu.new_version' => 'Доступна новая версия',
+    'top_menu.from' => 'от',
+
+    /**
+     *  Главное меню
+     */
+    'main_menu.home' => 'Главная',
+    'main_menu.news' => 'Новости',
+    'main_menu.network' => 'Сеть',
+    'main_menu.network.devices' => 'Устройства',
+
+    'main_menu.infrastructure' => 'Инфраструктура',
+    'main_menu.infrastructure.servers' => 'Серверы',
+    'main_menu.hosting' => 'Хостинг',
+    'main_menu.hosting.sites' => 'Сайты',
+
+    'main_menu.settings' => 'Настройки',
+    'main_menu.settings.users' => 'Пользователи',
+    'main_menu.settings.system' => 'Система',
+    'main_menu.settings.bridge' => 'Бридж',
+    'main_menu.settings.integrations' => 'Интеграции',
+
+    'main_menu.help' => 'Помощь',
+    'main_menu.documentation' => 'Документация',
+    'main_menu.changelog' => 'История версий',
+    'main_menu.contacts' => 'Контакты',
+
+    /**
+     * Меню "хлебные крошки"
+     */
+    'breadcrumbs.main' => 'Главная',
+    'breadcrumbs.network' => 'Сеть',
+
+
+    /**
+     * Карточка устройсва
+     */
+    'device.notifications_and_errors' => 'Уведомления и ошибки',
+    'device.errors' => 'Ошибки',
+    'device.snmp_fail' => 'SNMP. Ошибка подключения к устройству',
+    'device.model' => 'Модель',
+    'device.firmware_version' => 'Версия прошивки',
+    'device.date_time' => 'Дата / время',
+    'device.platform_type' => 'Тип платформы',
+    'device.port' => 'порт',
+    'device.ports' => 'Порты',
+];
