d6ea4ee12f8cd8794ab12964f0936099a76245b0
AleksandrGoriachev <bmw_318@mail.ru>
2020-01-14 01:21:54 +0300

    20200114_sm-6

--- resources/views/widgets/newpings.blade.php
+++ resources/views/widgets/newpings.blade.php
@@ -1,10 +1,10 @@
 @section('newpings')
-    <div class="col-6" id="2_newpings">
+    <div class="col-sm-6" id="2_newpings">
         <div class="card">
             <div class="card-header border-0">
                 <h3 class="card-title">
                     <i class="fas fa-map-marker-alt mr-1"></i>
-                    Результаты пинга последних 5 добавленных сайтов, мс
+                    Результаты пинга 5 новых сайтов, мс
                 </h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -17,7 +17,7 @@
             </div>
             <div class="card-body">
                 <canvas id="newSitesPings" data-title="{{$titles}}" data-ping="{{$pings}}" width="75%"
-                        height="50%">
+                        height="30%">
                 </canvas>
             </div>
         </div>
--- resources/views/widgets/newwebservers.blade.php
+++ resources/views/widgets/newwebservers.blade.php
@@ -1,10 +1,10 @@
 @section('newwebservers')
-    <div class="col-6" id="3_newwebservers">
+    <div class="col-sm-6" id="3_newwebservers">
         <div class="card">
             <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
                 <h3 class="card-title">
                     <i class="fas fa-map-marker-alt mr-1"></i>
-                    Статистика используемых версий для последних добавленных 5 web серверов, шт
+                    Статистика используемых версий для 5 новых web серверов, шт
                 </h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -18,7 +18,7 @@
             <div class="card-body">
                 <canvas id="newSitesWebServers" data-server="{{$servers}}" data-count="{{$counts}}"
                         width="75%"
-                        height="50%">
+                        height="30%">
                 </canvas>
             </div>
         </div>
