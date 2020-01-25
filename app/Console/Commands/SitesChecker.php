6e870755e0bdc48214fdf436e1b1fa1e9691e230
Anton Zheltyshev <az@maxrival.com>
2020-01-26 00:00:04 +0300

    Added forks.

--- app/Console/Commands/SitesChecker.php
+++ app/Console/Commands/SitesChecker.php
@@ -50,97 +50,103 @@ class SitesChecker extends Command
         }
 
         foreach ($sites as $site) {
-            try {
-                if ($site->checksList->use_file === 1) {
-                    $httpClient    = new Client();
-                    $url           = ($site->https === 1 && $site->checksList->check_https === 1) ? 'https://' . $site->file_url : 'http://' . $site->file_url;
-                    $request       = $httpClient->request('GET', $url, ['allow_redirects' => false]);
-                    $response      = $request->getBody();
-                    $responseArray = json_decode($response, true);
-                    $phpVersion    = $responseArray['php-version'];
-                    $statusCode    = $request->getStatusCode();
-                    $webServerType = $responseArray['web-server'];
-                    $phpBranch     = $responseArray['php-branch'];
-                } else {
-                    $httpClient    = new Client();
-                    $url           = ($site->https === 1 && $site->checksList->check_https === 1) ? 'https://' . $site->url : 'http://' . $site->url;
-                    $response      = $httpClient->request('GET', $url, ['allow_redirects' => false]);
-                    $phpVersion    = $response->getHeader('X-Powered-By');
-                    $webServerType = $response->getHeader('server');
+            $fork = pcntl_fork();
 
-                    if ($webServerType != null) {
-                        $webServerType = $webServerType[0];
+            if ($fork == -1) {
+                echo 'error';
+            } elseif($fork) {
+                try {
+                    if ($site->checksList->use_file === 1) {
+                        $httpClient    = new Client();
+                        $url           = ($site->https === 1 && $site->checksList->check_https === 1) ? 'https://' . $site->file_url : 'http://' . $site->file_url;
+                        $request       = $httpClient->request('GET', $url, ['allow_redirects' => false]);
+                        $response      = $request->getBody();
+                        $responseArray = json_decode($response, true);
+                        $phpVersion    = $responseArray['php-version'];
+                        $statusCode    = $request->getStatusCode();
+                        $webServerType = $responseArray['web-server'];
+                        $phpBranch     = $responseArray['php-branch'];
                     } else {
-                        $webServerType = 0;
-                    }
+                        $httpClient    = new Client();
+                        $url           = ($site->https === 1 && $site->checksList->check_https === 1) ? 'https://' . $site->url : 'http://' . $site->url;
+                        $response      = $httpClient->request('GET', $url, ['allow_redirects' => false]);
+                        $phpVersion    = $response->getHeader('X-Powered-By');
+                        $webServerType = $response->getHeader('server');
 
-                    if (preg_match('/^[0-9]*/', $response->getStatusCode())) {
-                        $statusCode = $response->getStatusCode();
-                    } else {
-                        $statusCode = 999;
-                    }
+                        if ($webServerType != null) {
+                            $webServerType = $webServerType[0];
+                        } else {
+                            $webServerType = 0;
+                        }
 
-                    if ($phpVersion != null) {
-                        if (preg_match('/^PHP/', $phpVersion[0])) {
-                            preg_match('/[0-9].[0-9].[0-9]/', $phpVersion[0], $rawPhpVersion);
-                            $phpVersion   = $rawPhpVersion[0];
-                            $phpBranchRaw = explode('.', $phpVersion);
-                            $phpBranchRaw = $phpBranchRaw[0] * 10000 + $phpBranchRaw[1] * 100 + $phpBranchRaw[2];
-                            $phpBranch    = Str::substr($phpBranchRaw, 0, 3);
+                        if (preg_match('/^[0-9]*/', $response->getStatusCode())) {
+                            $statusCode = $response->getStatusCode();
+                        } else {
+                            $statusCode = 999;
+                        }
+
+                        if ($phpVersion != null) {
+                            if (preg_match('/^PHP/', $phpVersion[0])) {
+                                preg_match('/[0-9].[0-9].[0-9]/', $phpVersion[0], $rawPhpVersion);
+                                $phpVersion   = $rawPhpVersion[0];
+                                $phpBranchRaw = explode('.', $phpVersion);
+                                $phpBranchRaw = $phpBranchRaw[0] * 10000 + $phpBranchRaw[1] * 100 + $phpBranchRaw[2];
+                                $phpBranch    = Str::substr($phpBranchRaw, 0, 3);
+                            } else {
+                                $phpVersion = 0;
+                                $phpBranch  = 0;
+                            }
                         } else {
                             $phpVersion = 0;
                             $phpBranch  = 0;
                         }
-                    } else {
-                        $phpVersion = 0;
-                        $phpBranch  = 0;
                     }
-                }
 
-                $ssl = new SitesSSLChecker();
-                $ssl->handle($site->id);
+                    $ssl = new SitesSSLChecker();
+                    $ssl->handle($site->id);
 
-                //   HTTP code saving process
-                $http = SitesHttpCodes::where('site_id', $site->id)->first();
-                if (isset($http)) {
-                    $http->http_code = $statusCode;
-                } else {
-                    $fillable = ['site_id' => $site->id, 'http_code' => $statusCode];
-                    $http     = new SitesHttpCodes($fillable);
-                }
-                $http->updated_at = Carbon::now();
-                $http->save();
+                    //   HTTP code saving process
+                    $http = SitesHttpCodes::where('site_id', $site->id)->first();
+                    if (isset($http)) {
+                        $http->http_code = $statusCode;
+                    } else {
+                        $fillable = ['site_id' => $site->id, 'http_code' => $statusCode];
+                        $http     = new SitesHttpCodes($fillable);
+                    }
+                    $http->updated_at = Carbon::now();
+                    $http->save();
 
-                //   WebServer type saving process
-                $webServer = SitesWebServers::where('site_id', $site->id)->first();
-                if (isset($webServer)) {
-                    $webServer->web_server = $webServerType;
-                } else {
-                    $fillable  = ['site_id' => $site->id, 'web_server' => $webServerType];
-                    $webServer = new SitesWebServers($fillable);
-                }
-                $webServer->updated_at = Carbon::now();
-                $webServer->save();
+                    //   WebServer type saving process
+                    $webServer = SitesWebServers::where('site_id', $site->id)->first();
+                    if (isset($webServer)) {
+                        $webServer->web_server = $webServerType;
+                    } else {
+                        $fillable  = ['site_id' => $site->id, 'web_server' => $webServerType];
+                        $webServer = new SitesWebServers($fillable);
+                    }
+                    $webServer->updated_at = Carbon::now();
+                    $webServer->save();
 
-                //    PHP version saving process
-                $php = SitesPhpVersions::where('site_id', $site->id)->first();
-                if (isset($php)) {
-                    $php->version = $phpVersion;
-                    $php->branch  = $phpBranch;
-                } else {
-                    $fillable = ['site_id' => $site->id, 'version' => $phpVersion, 'branch' => $phpBranch];
-                    $php      = new SitesPhpVersions($fillable);
-                }
-                $php->updated_at = Carbon::now();
-                $php->save();
+                    //    PHP version saving process
+                    $php = SitesPhpVersions::where('site_id', $site->id)->first();
+                    if (isset($php)) {
+                        $php->version = $phpVersion;
+                        $php->branch  = $phpBranch;
+                    } else {
+                        $fillable = ['site_id' => $site->id, 'version' => $phpVersion, 'branch' => $phpBranch];
+                        $php      = new SitesPhpVersions($fillable);
+                    }
+                    $php->updated_at = Carbon::now();
+                    $php->save();
 
-                // Now we remove pending status from site
-                $pending = Sites::where('id', $site->id)->first();
-                if ((int) ($pending->pending) === 1) {
-                    $pending->pending = 0;
-                    $pending->save();
+                    // Now we remove pending status from site
+                    $pending = Sites::where('id', $site->id)->first();
+                    if ((int) ($pending->pending) === 1) {
+                        $pending->pending = 0;
+                        $pending->save();
+                    }
+                } catch (\Exception $e) {
                 }
-            } catch (\Exception $e) {
             }
         }
 
