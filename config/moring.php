da8c7ed68797364516d600b9d1c06984deb58875
Anton Zheltyshev <az@maxrival.com>
2020-03-15 13:43:29 +0300

    Added human version.

--- config/moring.php
+++ config/moring.php
@@ -2,6 +2,7 @@
 
 return [
     'build' => '013',
+    'version' => '0.1.0',
     'createYear' => '2019',
     'bridgeUrl' => 'http://api.moring.ru/v1',
     'bridgeCurrentMoringVersionUrl' => '/moring/version',
