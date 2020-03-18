4626d28eba1894e844b1af766ab2491eaaa7d8bc
Anton Zheltyshev <az@maxrival.com>
2020-03-18 23:01:47 +0300

    Updating echo comments on commands.

--- commands/testing_local.sh
+++ commands/testing_local.sh
@@ -1,14 +1,26 @@
-echo 'Copy env to env.tmp'
+echo 'Copy .env to .env.tmp'
 cp .env .env.tmp
 
-echo 'Copy env testing to env'
+echo 'Copy .env.testing file to .env file'
 cp .env.testing.example .env
 
+echo 'Clearing artisan config'
 php artisan config:clear
+
+echo 'Artisan key generation'
 php artisan key:generate
 
+echo 'Starting PHPunit'
 ./vendor/bin/phpunit
+
+echo 'Removing .env file'
 rm -f .env
+
+echo 'Copy .env.tmp file to .env '
 cp .env.tmp .env
+
+echo 'Removing .env.tmp file'
 rm -f .env.tmp
+
+echo 'Caching artisan config'
 php artisan config:cache
