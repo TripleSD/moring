375ed7fe3341d5f8f66de923f3d091d21973bd92
Anton Zheltyshev <az@maxrival.com>
2020-03-18 23:05:25 +0300

    Refactoring for test.

--- app/Repositories/Devices/DevicesModelsRepository.php
+++ app/Repositories/Devices/DevicesModelsRepository.php
@@ -11,15 +11,27 @@ class DevicesModelsRepository extends Repository
      * @param string $modelTitle
      * @return int
      */
-    public function checkModel(string $modelTitle): int
+    public function getModelId(string $modelTitle): int
     {
-        $model = DevicesModels::where('title', $modelTitle)->first();
+        $model = $this->getModel($modelTitle);
 
         if (empty($model)) {
-            $model        = new DevicesModels();
-            $model->title = $modelTitle;
-            $model->save();
+            return $this->storeModel($modelTitle);
         }
+
+        return $model->id;
+    }
+
+    public function getModel(string $modelTitle)
+    {
+        return DevicesModels::where('title', $modelTitle)->first();
+    }
+
+    public function storeModel(string $modelTitle)
+    {
+        $model        = new DevicesModels();
+        $model->title = $modelTitle;
+        $model->save();
 
         return $model->id;
     }
