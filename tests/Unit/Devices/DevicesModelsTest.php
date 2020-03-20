eb0154a3700502ac2ea3f17d02a5820ba4a93fa8
Anton Zheltyshev <az@maxrival.com>
2020-03-20 22:09:46 +0300

    Removed unused code for private methods.

--- tests/Unit/Devices/DevicesModelsTest.php
+++ tests/Unit/Devices/DevicesModelsTest.php
@@ -7,28 +7,6 @@ use Tests\TestCase;
 
 class DevicesModelsTest extends TestCase
 {
-    public function testStoreFirmware(): void
-    {
-        $device = new DevicesModelsRepository();
-        $this->assertEquals(null, $device->getModel('TestModel1'));
-        $device->storeModel('TestModel1');
-        $this->assertEquals(1, $device->getModelId('TestModel1'));
-        $this->assertDatabaseHas('devices_models', ['title' => 'TestModel1']);
-        $this->assertEquals(2, $device->getModelId('TestModel2'));
-        $this->assertDatabaseHas('devices_models', ['title' => 'TestModel2']);
-    }
-
-    public function testGetModel(): void
-    {
-        $device = new DevicesModelsRepository();
-        $this->assertEquals(null, $device->getModel('TestModel1'));
-        $this->assertDatabaseMissing('devices_models', ['title' => 'TestModel1']);
-        $this->assertEquals(1, $device->getModelId('TestModel1'));
-        $this->assertDatabaseHas('devices_models', ['title' => 'TestModel1']);
-        $this->assertEquals(null, $device->getModel('TestModel2'));
-        $this->assertDatabaseMissing('devices_models', ['title' => 'TestModel2']);
-    }
-
     public function testGetModelId(): void
     {
         $device = new DevicesModelsRepository();
--- tests/Unit/Devices/FirmwareTest.php
+++ tests/Unit/Devices/FirmwareTest.php
@@ -7,28 +7,6 @@ use Tests\TestCase;
 
 class FirmwareTest extends TestCase
 {
-    public function testStoreFirmware(): void
-    {
-        $device = new DevicesFirmwaresRepository();
-        $this->assertEquals(null, $device->getFirmware('TestVendor1'));
-        $device->storeFirmware('TestVendor1');
-        $this->assertEquals(1, $device->getFirmwareId('TestVendor1'));
-        $this->assertDatabaseHas('devices_firmwares', ['title' => 'TestVendor1']);
-        $this->assertEquals(2, $device->getFirmwareId('TestVendor2'));
-        $this->assertDatabaseHas('devices_firmwares', ['title' => 'TestVendor2']);
-    }
-
-    public function testGetFirmware(): void
-    {
-        $device = new DevicesFirmwaresRepository();
-        $this->assertEquals(null, $device->getFirmware('TestVendor1'));
-        $this->assertDatabaseMissing('devices_firmwares', ['title' => 'TestVendor1']);
-        $this->assertEquals(1, $device->getFirmwareId('TestVendor1'));
-        $this->assertDatabaseHas('devices_firmwares', ['title' => 'TestVendor1']);
-        $this->assertEquals(null, $device->getFirmware('TestVendor2'));
-        $this->assertDatabaseMissing('devices_firmwares', ['title' => 'TestVendor2']);
-    }
-
     public function testGetFirmwareId(): void
     {
         $device = new DevicesFirmwaresRepository();
