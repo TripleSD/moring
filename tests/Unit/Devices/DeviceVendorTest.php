f980f58337a63ae8a8d4da941dbd5938129da1d0
Anton Zheltyshev <az@maxrival.com>
2020-03-20 13:49:30 +0300

    Added new test.

---
+++ tests/Unit/Devices/DeviceVendorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Devices;
+
+use Tests\TestCase;
+use App\Repositories\Devices\DevicesVendorsRepository;
+
+class DeviceVendorTest extends TestCase
+{
+    public function testStoreVendor(): void
+    {
+        $device = new DevicesVendorsRepository();
+        $this->assertEquals(null, $device->getVendor('TestVendor1'));
+        $device->storeVendor('TestVendor1');
+        $this->assertEquals(1, $device->getVendorId('TestVendor1'));
+        $this->assertDatabaseHas('devices_vendors', ['title' => 'TestVendor1']);
+    }
+
+    public function testGetVendor(): void
+    {
+        $device = new DevicesVendorsRepository();
+        $this->assertEquals(null, $device->getVendor('TestVendor1'));
+        $this->assertDatabaseMissing('devices_vendors', ['title' => 'TestVendor1']);
+        $this->assertEquals(1, $device->getVendorId('TestVendor1'));
+        $this->assertDatabaseHas('devices_vendors', ['title' => 'TestVendor1']);
+        $this->assertDatabaseMissing('devices_vendors', ['title' => 'TestVendor2']);
+    }
+
+    public function testGetVendorId(): void
+    {
+        $device = new DevicesVendorsRepository();
+        $this->assertEquals(1, $device->getVendorId('TestVendor1'));
+        $this->assertEquals(1, $device->getVendorId('TestVendor1'));
+        $this->assertEquals(2, $device->getVendorId('TestVendor2'));
+    }
+}
