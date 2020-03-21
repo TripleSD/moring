92b95bf99795e1ed4aa0bafb8c78a69ead9c7c24
Anton Zheltyshev <az@maxrival.com>
2020-03-21 23:25:42 +0300

    Refactoring & code style fix.

--- app/Repositories/Snmp/SnmpRepository.php
+++ app/Repositories/Snmp/SnmpRepository.php
@@ -11,27 +11,7 @@ class SnmpRepository extends Repository
     {
         // TODO восстановить try/catch
         $snmpFlow = new SNMP(SNMP::VERSION_2c, $host, $community);
+
         return $snmpFlow->walk(".");
-    }
-
-    public function getVendor(array $snmpFlow): string
-    {
-        if (preg_match('/RouterOS/', $snmpFlow['SNMPv2-MIB::sysDescr.0'])) {
-            $vendor = 'Mikrotik';
-        }
-
-        if (preg_match('/Cisco/', $snmpFlow['SNMPv2-MIB::sysDescr.0'])) {
-            $vendor = 'Cisco';
-        }
-
-        if (preg_match('/[D][EG][S]/', $snmpFlow['SNMPv2-MIB::sysDescr.0'])) {
-            $vendor = 'D-Link';
-        }
-
-        if (preg_match('/MES/', $snmpFlow['SNMPv2-MIB::sysDescr.0'])) {
-            $vendor = 'Eltex';
-        }
-
-        return (string) $vendor;
     }
 }
