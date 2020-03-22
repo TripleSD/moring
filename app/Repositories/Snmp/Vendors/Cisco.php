e91d7d8bf98b24e462596b5a59472c659814a9a9
Anton Zheltyshev <az@maxrival.com>
2020-03-22 12:30:58 +0300

    Fix.

--- app/Repositories/Snmp/Vendors/Cisco.php
+++ app/Repositories/Snmp/Vendors/Cisco.php
@@ -61,7 +61,7 @@ class Cisco implements VendorInterface
      */
     public function getUptime($snmpArray): string
     {
-        $string = $snmpArray['.1.3.6.1.2.1.1.3.0'];
+        $string = $snmpArray['DISMAN-EVENT-MIB::sysUpTimeInstance'];
         preg_match('/\((\d+)\)/', $string, $string);
         preg_match('/\d+/', $string[1], $string);
 
@@ -99,8 +99,9 @@ class Cisco implements VendorInterface
     public function getSerialNumber($snmpArray): ?string
     {
         try {
-            $string = $snmpArray['.1.3.6.1.2.1.47.1.1.1.1.11.1001'];
+            $string = $snmpArray['SNMPv2-SMI::mib-2.47.1.1.1.1.11.1'];
             $string = str_replace('STRING: ', '', $string);
+            $string = str_replace('"', '', $string);
 
             return (string) trim($string);
         } catch (\Exception $e) {
