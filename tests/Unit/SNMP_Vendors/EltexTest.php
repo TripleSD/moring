0b7efb3f8603c4ed537274743f6c0b77a4618014
Anton Zheltyshev <az@maxrival.com>
2020-03-27 12:22:49 +0300

    Added new SNMP test.

---
+++ tests/Unit/SNMP_Vendors/EltexTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\SNMP_Vendors;
+
+use App\Repositories\Snmp\Vendor;
+use App\Repositories\Snmp\Vendors\Eltex;
+use App\Repositories\Snmp\SnmpRepository;
+use Tests\TestCase;
+
+class EltexTest extends TestCase
+{
+    public function testParseName(): void
+    {
+        $snmpObject          = new SnmpRepository();
+        $snmpFlow            = $snmpObject->startSession($this->setVarsConnection());
+        $vendorNameRawString = $snmpObject->getVendorNameRawString($snmpFlow);
+
+        $vendor = new Vendor();
+        $this->assertEquals('Eltex', $vendor->parseName($vendorNameRawString));
+    }
+
+    public function testGetVendorClass(): void
+    {
+        $class = new Eltex();
+        $this->assertEquals($class, (new Vendor())->getVendorClass('Eltex'));
+    }
+
+    private function setVarsConnection()
+    {
+        $array['hostname']      = '127.0.0.1:9004';
+        $array['snmpCommunity'] = 'public';
+
+        return $array;
+    }
+}
