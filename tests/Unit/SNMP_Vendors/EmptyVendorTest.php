9032f66aab3d45fb8e30ecfa69bdbdd950aa9970
Anton Zheltyshev <az@maxrival.com>
2020-03-27 12:22:58 +0300

    Added new SNMP test.

---
+++ tests/Unit/SNMP_Vendors/EmptyVendorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit\SNMP_Vendors;
+
+use App\Repositories\Snmp\Vendor;
+use App\Repositories\Snmp\SnmpRepository;
+use Tests\TestCase;
+
+class EmptyVendorTest extends TestCase
+{
+    public function testParseName(): void
+    {
+        $snmpObject          = new SnmpRepository();
+        $snmpFlow            = $snmpObject->startSession($this->setVarsConnection());
+        $vendorNameRawString = $snmpObject->getVendorNameRawString($snmpFlow);
+
+        $vendor = new Vendor();
+        $this->assertEquals(null, $vendor->parseName($vendorNameRawString));
+    }
+
+    private function setVarsConnection()
+    {
+        $array['hostname']      = '127.0.0.1:9006';
+        $array['snmpCommunity'] = 'public';
+
+        return $array;
+    }
+}
