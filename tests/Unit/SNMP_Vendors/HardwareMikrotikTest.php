8e3f8fbe6c1b5f67428ccf692d3d9c4f4efb214f
Anton Zheltyshev <az@maxrival.com>
2020-03-27 12:23:04 +0300

    Added new SNMP test.

---
+++ tests/Unit/SNMP_Vendors/HardwareMikrotikTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\SNMP_Vendors;
+
+use App\Repositories\Snmp\Vendor;
+use App\Repositories\Snmp\SnmpRepository;
+use Tests\TestCase;
+use App\Repositories\Snmp\Vendors\Mikrotik;
+
+class HardwareMikrotikTest extends TestCase
+{
+    public function testParseName(): void
+    {
+        $snmpObject          = new SnmpRepository();
+        $snmpFlow            = $snmpObject->startSession($this->setVarsConnection());
+        $vendorNameRawString = $snmpObject->getVendorNameRawString($snmpFlow);
+
+        $vendor = new Vendor();
+        $this->assertEquals('Mikrotik', $vendor->parseName($vendorNameRawString));
+    }
+
+    public function testGetVendorClass(): void
+    {
+        $class = new Mikrotik();
+        $this->assertEquals($class, (new Vendor())->getVendorClass('Mikrotik'));
+    }
+
+    private function setVarsConnection()
+    {
+        $array['hostname']      = '127.0.0.1:9005';
+        $array['snmpCommunity'] = 'public';
+
+        return $array;
+    }
+}
