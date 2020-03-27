6d82542d977f1b949ecf8b9216dddf8244fcf2e0
Anton Zheltyshev <az@maxrival.com>
2020-03-27 12:21:40 +0300

    Added new SNMP test.

---
+++ tests/Unit/SNMP_Vendors/CiscoTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\SNMP_Vendors;
+
+use App\Repositories\Snmp\Vendor;
+use App\Repositories\Snmp\Vendors\Cisco;
+use App\Repositories\Snmp\SnmpRepository;
+use Tests\TestCase;
+
+class CiscoTest extends TestCase
+{
+    public function testParseName(): void
+    {
+        $snmpObject          = new SnmpRepository();
+        $snmpFlow            = $snmpObject->startSession($this->setVarsConnection());
+        $vendorNameRawString = $snmpObject->getVendorNameRawString($snmpFlow);
+
+        $vendor = new Vendor();
+        $this->assertEquals('Cisco', $vendor->parseName($vendorNameRawString));
+    }
+
+    public function testGetVendorClass(): void
+    {
+        $vendor = new Vendor();
+        $this->assertInstanceOf(Cisco::class, $vendor->getVendorClass('Cisco'));
+    }
+
+    private function setVarsConnection()
+    {
+        $array['hostname']      = '127.0.0.1:9002';
+        $array['snmpCommunity'] = 'public';
+
+        return $array;
+    }
+}
