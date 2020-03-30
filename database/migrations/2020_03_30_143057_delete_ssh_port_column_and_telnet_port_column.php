b92d3f83bad737a5cf20c171cfd6cbe8ba651c8c
Anton Zheltyshev <az@maxrival.com>
2020-03-30 15:20:02 +0300

    Fixed multiple calls in a single modification.

---
+++ database/migrations/2020_03_30_143057_delete_ssh_port_column_and_telnet_port_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteSshPortColumnAndTelnetPortColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'devices',
+            function (Blueprint $table) {
+                $table->integer('port_ssh')->nullable()->after('snmp_version');
+                $table->integer('port_telnet')->nullable()->after('port_ssh');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- database/migrations/2020_03_30_143258_create_port_ssh_column_and_port_telnet_column.php
+++ database/migrations/2020_03_30_143258_create_port_ssh_column_and_port_telnet_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameSshPortColumnAndTelnetPortColumn extends Migration
+class CreatePortSshColumnAndPortTelnetColumn extends Migration
 {
     /**
      * Run the migrations.
