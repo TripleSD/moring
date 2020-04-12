73a99b301b7039c1f05ae6a275913802fd114790
Anton Zheltyshev <az@maxrival.com>
2020-04-12 09:48:33 +0300

    New version 0.1.6 (build 026)

--- config/moring.php
+++ config/moring.php
@@ -2,7 +2,7 @@
 
 return [
     'build' => '026',
-    'version' => '0.1.5',
+    'version' => '0.1.6',
     'createYear' => '2019',
     'bridgeUrl' => 'http://api.moring.ru/v1',
     'bridgeCurrentMoringVersionUrl' => '/moring/version',
