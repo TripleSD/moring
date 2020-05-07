2c7ae61e9e50a3694330bb4f8230d0e5b671f46e
Anton Zheltyshev <az@maxrival.com>
2020-05-07 17:54:47 +0300

    Updating information.

--- config/moring.php
+++ config/moring.php
@@ -2,7 +2,7 @@
 
 return [
     'build' => '028',
-    'version' => '0.1.7',
+    'version' => '0.1.8',
     'createYear' => '2019',
     'bridgeUrl' => 'http://api.moring.ru/v1',
     'bridgeCurrentMoringVersionUrl' => '/moring/version',
