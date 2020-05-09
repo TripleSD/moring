ca8f21e245072fb68a450bfbe377d93b20eb3291
Anton Zheltyshev <az@maxrival.com>
2020-05-09 15:12:00 +0300

    Code style fix.

--- app/Repositories/BridgeRepository.php
+++ app/Repositories/BridgeRepository.php
@@ -13,20 +13,25 @@ class BridgeRepository extends Repository
     {
         $httpClient = new Client();
         // Url getting from /config/moring.php
-        $url = Config::get('moring.bridgeUrl') . Config::get(
+        $url      = Config::get('moring.bridgeUrl') . Config::get(
                 'moring.bridgeCreateIdentificatorUrl'
             );
-        $response = $httpClient->request('GET', $url,
-                                         ['query' => ['env' => env('APP_ENV')],
-                                         'allow_redirects' => false]);
+        $response = $httpClient->request(
+            'GET',
+            $url,
+            [
+                'query' => ['env' => env('APP_ENV')],
+                'allow_redirects' => false,
+            ]
+        );
 
         return json_decode($response->getBody(), true);
     }
 
     public function getBridgeInfo($identificator)
     {
-        $httpClient = new Client();
-        $response = $httpClient->request(
+        $httpClient  = new Client();
+        $response    = $httpClient->request(
             'GET',
             Config::get('moring.bridgeUrl') . Config::get('moring.bridgeCurrentVersionUrl'),
             ['query' => ['identificator' => $identificator], 'allow_redirects' => false]
