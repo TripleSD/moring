1526cc248f96f129df5cb8cb5135dec494aa936e
Anton Zheltyshev <az@maxrival.com>
2020-05-28 15:35:00 +0300

    Added new template.

---
+++ resources/views/admin/backups/ftp/create.blade.php
@@ -0,0 +1,140 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-4">
+                    <h1 class="m-0 text-dark">Backups</h1>
+                </div>
+                <div class="col-sm-8">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{route('home')}}">Главная</a></li>
+                        <li class="breadcrumb-item">Backup</li>
+                        <li class="breadcrumb-item active">Ftp</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Редактирование FPT проверки</h3>
+                                <div class="card-tools">
+                                    <a href="{{route('network.devices.index')}}"
+                                       class="btn btn-sm bg-gradient-info" title="Вернуться">
+                                        <i class="fa fa-arrow-left"></i></a>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <div class="col-sm-6">
+                                    {{ Form::open([ 'url' => route('backups.ftp.store'), 'method' => 'post']) }}
+                                    <div class="form-group">
+                                        <b>Описание</b>
+                                        {{ Form::text('description', null , ['class' => 'form-control', 'required', 'placeholder' => 'mydevice.local или 192.168.88.1']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Сетевое имя устройства или IP адрес</b>
+                                        {{ Form::text('hostname', null , ['class' => 'form-control', 'required', 'placeholder' => 'mydevice.local или 192.168.88.1']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Порт</b>
+                                        {{ Form::text('port', null, ['class' => 'form-control', 'required', 'placeholder' => 'Пример: 161']) }}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Папка</b>
+                                        {{Form::text('folder', null, ['class' => 'form-control', 'required', 'placeholder' => 'Пример: public'])}}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Pre suffix</b>
+                                        {{Form::text('pre', null, ['class' => 'form-control', 'placeholder' => 'Пример: 22'])}}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Post suffix</b>
+                                        {{Form::text('post', null, ['class' => 'form-control', 'placeholder' => 'Пример: 23'])}}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>File</b>
+                                        {{Form::text('filename', null, ['class' => 'form-control', 'placeholder' => 'Пример: http://127.0.0.1'])}}
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <b>Interval</b>
+                                        <div>
+                                            {{ Form::radio('interval', '1', false) }} 1h
+                                            {{ Form::radio('interval', '3', false) }} 3h
+                                            {{ Form::radio('interval', '6', false) }} 6h
+                                            {{ Form::radio('interval', '12',false) }} 12h
+                                        </div>
+                                        <details class="mt--3 small">
+                                            <summary>
+                                                Дополнительная информация
+                                            </summary>
+                                            ...
+                                        </details>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-xs bg-gradient-cyan">Добавить</button>
+                                    {{ Form::close() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
