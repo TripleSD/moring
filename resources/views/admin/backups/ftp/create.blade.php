2ebb41105d7d1d1838b92f424d6f6b240911c537
Anton Zheltyshev <az@maxrival.com>
2020-05-30 18:12:50 +0300

    Refactoring template.

--- resources/views/admin/backups/ftp/create.blade.php
+++ resources/views/admin/backups/ftp/create.blade.php
@@ -23,7 +23,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Редактирование FTP проверки</h3>
+                                <h3 class="card-title">Создание FTP проверки</h3>
                                 <div class="card-tools">
                                     <a href="{{route('backups.ftp.index')}}"
                                        class="btn btn-sm bg-gradient-info" title="Вернуться">
