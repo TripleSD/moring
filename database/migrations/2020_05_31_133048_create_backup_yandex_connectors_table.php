d5390bc32f9e4501175e6c0115360ae4f63f7f19
Anton Zheltyshev <az@maxrival.com>
2020-05-31 19:04:56 +0300

    Added new migration.

---
+++ database/migrations/2020_05_31_133048_create_backup_yandex_connectors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBackupYandexConnectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('backup_yandex_connectors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description')->comment('Description');
+            $table->string('token')->comment('Token');
+            $table->text('comment')->nullable()->comment('Comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('backup_yandex_connections');
+    }
+}
