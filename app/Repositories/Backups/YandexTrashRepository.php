a948ed7e857aa0458f17fad12cb77da282b30193
Anton Zheltyshev <az@maxrival.com>
2020-05-31 22:59:20 +0300

    Added new repository.

---
+++ app/Repositories/Backups/YandexTrashRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Backups;
+
+use App\Repositories\Repository;
+
+/**
+ * Class YandexTrashRepository
+ * @package App\Repositories\Backups
+ */
+class YandexTrashRepository extends Repository
+{
+    /**
+     * @return mixed
+     */
+    public function getList()
+    {
+//        return BackupYandexConnectors::get();
+    }
+}
