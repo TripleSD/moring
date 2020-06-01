2b7f254f0acfd918cee9f673be413dd6b25d1102
Anton Zheltyshev <az@maxrival.com>
2020-06-01 17:29:02 +0300

    Added new function.

--- app/Repositories/Backups/YandexTrashRepository.php
+++ app/Repositories/Backups/YandexTrashRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Backups;
 
 use App\Repositories\Repository;
+use App\Models\BackupYandexConnectors;
 
 /**
  * Class YandexTrashRepository
@@ -16,5 +17,30 @@ class YandexTrashRepository extends Repository
     public function getList()
     {
 //        return BackupYandexConnectors::get();
+    }
+
+    /**
+     * @param $request
+     * @return bool
+     */
+    public function cleanTrash($request)
+    {
+        $connector = BackupYandexConnectors::find($request->id);
+
+        $ch = curl_init('https://cloud-api.yandex.net/v1/disk/trash/resources/?path=');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: OAuth ' . $connector->token]);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        curl_close($ch);
+
+        if ($httpcode !== 202) {
+           return false;
+        }
+
+        return true;
     }
 }
