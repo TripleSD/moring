24b42e5c289df846a8decc64e568ed28156495c2
Anton Zheltyshev <az@maxrival.com>
2020-06-01 12:12:52 +0300

    Added new repository.

---
+++ app/Repositories/Backups/YandexTaskRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories\Backups;
+
+use App\Repositories\Repository;
+use App\Models\BackupYandexList;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BackupYandexRepository
+ * @package App\Repositories\Backups
+ */
+class YandexTaskRepository extends Repository
+{
+    /**
+     * @return Builder[]|Collection
+     */
+    public function tasksList()
+    {
+        return BackupYandexList::with(
+                [
+                    'logs' => function ($q) {
+                        return $q->where('resolved', 0);
+                    },
+                ]
+            )
+            ->get();
+    }
+}
