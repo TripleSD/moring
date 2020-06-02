250e220d1a51a56768e3fb5de2579c151f79a35d
Anton Zheltyshev <az@maxrival.com>
2020-06-02 19:26:52 +0300

    Code style fix.

--- app/Models/BackupYandexTask.php
+++ app/Models/BackupYandexTask.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * Class BackupYandexList
- * @package App\Models
  */
 class BackupYandexTask extends Model
 {
