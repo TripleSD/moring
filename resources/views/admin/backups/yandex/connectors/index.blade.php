6156ad5af355482ed660ba78db26d1c4fd2d7649
Anton Zheltyshev <az@maxrival.com>
2020-06-02 16:38:39 +0300

    Updating template.

--- resources/views/admin/backups/yandex/connectors/index.blade.php
+++ resources/views/admin/backups/yandex/connectors/index.blade.php
@@ -41,72 +41,105 @@
                             <div class="col-6">
                                 <div class="card">
                                     <div class="card-body">
-                                        <div class="small">
-                                            Краткое описание:
-                                            <b>{{ $connector->description }}</b>
-                                            <span class="btn-group float-right">
-                                            <a href="{{route('backups.yandex.connectors.show',$connector->id)}}"
-                                               class="btn btn-xs bg-info"
-                                               title="Просмотр устройства">
-                                                <i class="fa fa-eye"></i>
-                                            </a>
-                                            <a href="{{route('backups.yandex.connectors.edit', $connector->id)}}"
-                                               class="btn btn-xs bg-warning"
-                                               title="Редактирование устройства">
-                                                <i class="fas fa-pencil-alt"></i></a>
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="row small">
+                                                    <div class="col-6">
+                                                        <b>Идентификатор:</b>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        #{{ $connector->id }}
+                                                    </div>
+                                                </div>
+                                                <div class="row small">
+                                                    <div class="col-6">
+                                                        <b>Краткое описание:</b>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        {{ $connector->description }}
+                                                    </div>
+                                                </div>
+                                                <div class="row small">
+                                                    <div class="col-6">
+                                                        <b>Статус:</b>
+                                                    </div>
+                                                    <div class="col-6">
+                                                                <span
+                                                                    class="badge {{ ($connector->status) ? 'badge-success' : 'badge-gray' }}">
+                                                                    Активен
+                                                                </span>
+                                                        {{ $connector->http_code }}
+                                                    </div>
+                                                </div>
+                                                <div class="row small">
+                                                    <div class="col-6">
+                                                        <b>Последний опрос:</b>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        {{ $connector->status_updated_at }}
+                                                    </div>
+                                                </div>
 
-                                            <a href="{{route('backups.yandex.backups.yandex.connectors.refresh', $connector->id)}}"
-                                               class="btn btn-xs bg-success"
-                                               title="Обновить данные">
-                                                <i class="fas fa-sync-alt"></i></a>
+                                                <hr/>
 
-                                            @if($connector->trash_size > 0)
-                                                    <a href="{{route('backups.yandex.backups.yandex.connectors.clean', $connector->id)}}"
-                                                       class="btn btn-xs bg-danger"
-                                                       title="Очистка корзины">
-                                                    <i class="fas fa-dumpster-fire"></i></a>
-                                                @endif
-                                        </span>
-                                        </div>
-                                        <div class="small">
-                                            Статус:
-                                            <span
-                                                class="badge {{ ($connector->status) ? 'badge-success' : 'badge-gray' }}">
-                                                    Активен
-                                                </span>
-                                            {{ $connector->http_code }}
-                                        </div>
-                                        <div class="small">
-                                            Последний опрос: {{ $connector->status_updated_at }}
-                                        </div>
-                                        <hr/>
-                                        <div class="small">
-                                            <i class="fas fa-hdd"></i>
-                                            Total: {{ $connector->used_space / 1024 / 1024 / 1024}} Гб из
-                                            {{ $connector->total_space / 1024 / 1024 / 1024 }} Гб
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-primary"
-                                                     role="progressbar"
-                                                     aria-valuenow="{{ $connector->percent_used }}"
-                                                     aria-valuemin="0" aria-valuemax="100"
-                                                     style="width: {{ $connector->percent_used }}%">
+                                                <div class="row small">
+                                                    <div class="col-12">
+                                                        <i class="fas fa-hdd"></i>
+                                                        Total: {{ $connector->used_space / 1024 / 1024 / 1024}} Гб из
+                                                        {{ $connector->total_space / 1024 / 1024 / 1024 }} Гб
+                                                        <div class="progress">
+                                                            <div class="progress-bar bg-gradient-primary"
+                                                                 role="progressbar"
+                                                                 aria-valuenow="{{ $connector->percent_used }}"
+                                                                 aria-valuemin="0" aria-valuemax="100"
+                                                                 style="width: {{ $connector->percent_used }}%">
                                                         <span
                                                             class="text-dark">{{ $connector->percentused }}% Used</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                        <div class="small">
-                                            <i class="fas fa-dumpster"></i>
-                                            Trash: {{ $connector->trash_size / 1024 / 1024 / 1024}} Гб из
-                                            {{ $connector->total_space / 1024 / 1024 / 1024 }} Гб
-                                            <div class="progress">
-                                                <div class="progress-bar bg-primary" role="progressbar"
-                                                     aria-valuenow="{{ $connector->percent_bucket_used }}"
-                                                     aria-valuemin="0" aria-valuemax="100"
-                                                     style="width: {{ $connector->percent_bucket_used }}%">
+                                                <div class="row small">
+                                                    <div class="col-12">
+                                                        <i class="fas fa-dumpster"></i>
+                                                        Trash: {{ $connector->trash_size / 1024 / 1024 / 1024}} Гб из
+                                                        {{ $connector->total_space / 1024 / 1024 / 1024 }} Гб
+                                                        <div class="progress">
+                                                            <div class="progress-bar bg-primary" role="progressbar"
+                                                                 aria-valuenow="{{ $connector->percent_bucket_used }}"
+                                                                 aria-valuemin="0" aria-valuemax="100"
+                                                                 style="width: {{ $connector->percent_bucket_used }}%">
                                                     <span
                                                         class="text-dark">{{ $connector->percent_bucket_used }}% Used</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
+                                            </div>
+                                            <div class="col-4">
+                                                    <span class="btn-group float-right">
+                                                <a href="{{route('backups.yandex.connectors.show',$connector->id)}}"
+                                                   class="btn btn-xs bg-info"
+                                                   title="Просмотр устройства">
+                                                    <i class="fa fa-eye"></i>
+                                                </a>
+                                                <a href="{{route('backups.yandex.connectors.edit', $connector->id)}}"
+                                                   class="btn btn-xs bg-warning"
+                                                   title="Редактирование устройства">
+                                                    <i class="fas fa-pencil-alt"></i></a>
+
+                                                <a href="{{route('backups.yandex.backups.yandex.connectors.refresh', $connector->id)}}"
+                                                   class="btn btn-xs bg-success"
+                                                   title="Обновить данные">
+                                                    <i class="fas fa-sync-alt"></i></a>
+
+                                                @if($connector->trash_size > 0)
+                                                            <a href="{{route('backups.yandex.backups.yandex.connectors.clean', $connector->id)}}"
+                                                               class="btn btn-xs bg-danger"
+                                                               title="Очистка корзины">
+                                                        <i class="fas fa-dumpster-fire"></i></a>
+                                                        @endif
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
