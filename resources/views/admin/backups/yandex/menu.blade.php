b1e0e4435e3f25a8865b8b696fbb195fdb813b7b
Anton Zheltyshev <az@maxrival.com>
2020-06-02 23:07:57 +0300

    Fixed urls.

--- resources/views/admin/backups/yandex/menu.blade.php
+++ resources/views/admin/backups/yandex/menu.blade.php
@@ -8,8 +8,8 @@
        href="{{ route('backups.yandex.tasks.index') }}" title="Просмотр устройства">
         Проверки
     </a>
-    <a class="btn btn-xs {{ (request()->route()->named('backups.yandex.trash.index')) ? 'btn-danger' : 'btn-dark' }}"
-       href="{{ route('backups.yandex.trash.index') }}"
+    <a class="btn btn-xs {{ (request()->route()->named('backups.yandex.baskets.index')) ? 'btn-danger' : 'btn-dark' }}"
+       href="{{ route('backups.yandex.baskets.index') }}"
        title="Редактирование устройства">
         Корзины</a>
 </div>
