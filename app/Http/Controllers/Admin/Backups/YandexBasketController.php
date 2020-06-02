6917117f6d2dcec8b23daaaec23cbffbd0b8c29f
Anton Zheltyshev <az@maxrival.com>
2020-06-02 22:59:29 +0300

    Renaming file.

---
+++ app/Http/Controllers/Admin/Backups/YandexBasketController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Backups;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Backups\YandexBasketRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
+
+/**
+ * Class YandexBasketsController.
+ */
+class YandexBasketController extends Controller
+{
+    private $yandexBasketRepository;
+
+    public function __construct()
+    {
+        $this->yandexBasketRepository = new YandexBasketRepository();
+    }
+
+    /**
+     * @param YandexBasketRepository $yandexBasketRepository
+     * @return Application|Factory|View
+     */
+    public function index(YandexBasketRepository $yandexBasketRepository)
+    {
+        $trash = $yandexBasketRepository->getList();
+
+        return view('admin.backups.yandex.trash.index', compact('trash'));
+    }
+}
