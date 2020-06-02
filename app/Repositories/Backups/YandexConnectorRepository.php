35701fb1f5b981c8d190e44ae8fc8688d66f2726
Anton Zheltyshev <az@maxrival.com>
2020-06-02 13:03:26 +0300

    Added new function.

--- app/Repositories/Backups/YandexConnectorRepository.php
+++ app/Repositories/Backups/YandexConnectorRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Backups;
 
+use Carbon\Carbon;
 use App\Repositories\Repository;
 use App\Models\BackupYandexConnectors;
 
@@ -17,5 +18,44 @@ class YandexConnectorRepository extends Repository
     public function getList()
     {
         return BackupYandexConnectors::get();
+    }
+
+    public function refresh($request)
+    {
+        $connector = BackupYandexConnectors::find($request->id);
+
+        $ch = curl_init('https://cloud-api.yandex.net/v1/disk/');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: OAuth ' . $connector->token]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $res      = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+        curl_close($ch);
+
+        $res = json_decode($res, true);
+
+        if ($httpcode === 200) {
+            BackupYandexConnectors::where('id', $connector->id)->update(
+                [
+                    'total_space' => $res['total_space'],
+                    'trash_size' => $res['trash_size'],
+                    'used_space' => $res['used_space'],
+                    'http_code' => $httpcode,
+                    'status' => 1,
+                    'status_updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]
+            );
+            return true;
+        }
+
+        BackupYandexConnectors::where('id', $connector->id)->update(
+            [
+                'http_code' => $httpcode,
+                'status' => 0,
+                'status_updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        );
+        return false;
     }
 }
