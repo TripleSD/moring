922a4fecb7a1ee13710cbe8ad9e6c0478fc99615
Anton Zheltyshev <az@maxrival.com>
2020-06-02 17:22:57 +0300

    Updating template.

--- resources/views/admin/backups/yandex/tasks/create.blade.php
+++ resources/views/admin/backups/yandex/tasks/create.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class=" text-dark"><i class="nav-icon fas fa-box-open"></i> Backups</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-                        <li class="breadcrumb-item">Backups</li>
-                        <li class="breadcrumb-item active">Ftp</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <div class="card">
+                    <div class="card mt-3">
                         <div class="card-header">
-                            <h3 class="card-title">Создание FTP проверки</h3>
+                            <div class="card-title">
+                                <div class="btn-group">
+                                    <a href="{{route('home')}}"
+                                       class="btn btn-xs btn-outline-secondary" title="Вернуться">
+                                        <i class="fa fa-home"></i></a>
+                                </div>
+                                <div class="btn-group">
+                                    <span class="text-muted text-sm">Dashboard</span>
+                                    <span class="text-muted text-sm px-1"><i class="fas fa-chevron-right"></i></span>
+                                    <span class="text-muted text-sm">Backup</span>
+                                    <span class="text-muted text-sm px-1"><i class="fas fa-chevron-right"></i></span>
+                                    <span class="text-muted text-sm">
+                                        <a href="{{ route('backups.yandex.tasks.index') }}">Яндекс Диск</a>
+                                    </span>
+                                    <span class="text-muted text-sm px-1"><i class="fas fa-chevron-right"></i></span>
+                                    <span class="text-sm">Новая проверка</span>
+                                </div>
+                            </div>
                             <div class="card-tools">
-                                <a href="{{route('backups.ftp.index')}}"
-                                   class="btn btn-sm bg-gradient-info" title="Вернуться">
-                                    <i class="fa fa-arrow-left"></i></a>
+                                <a href="{{route('backups.yandex.tasks.index')}}"
+                                   class="btn btn-xs bg-gradient-info" title="Вернуться">
+                                    <i class="fa fa-arrow-left"></i> Назад</a>
                             </div>
                         </div>
                     </div>
@@ -43,10 +43,9 @@
                                 <span class="small text-danger float-right">
                                     * - обязательно для заполнения
                                 </span>
-
-                                {{ Form::open([ 'url' => route('backups.ftp.store'), 'method' => 'post']) }}
+                                {{ Form::open([ 'url' => route('backups.yandex.tasks.store'), 'method' => 'post']) }}
                                 <div class="form-group">
-                                    <b>Описание</b>
+                                    <b>Краткое описание</b>
                                     <span class="small text-danger">*</span>
                                     {{ Form::text('description', null , ['class' => 'form-control', 'required', 'placeholder' => 'mydevice.local или 192.168.88.1']) }}
                                     <details class="mt--3 small">
@@ -58,44 +57,9 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <b>Сетевое имя устройства или IP адрес</b>
+                                    <b>Коннектор</b>
                                     <span class="small text-danger">*</span>
-                                    {{ Form::text('hostname', null , ['class' => 'form-control', 'required', 'placeholder' => 'mydevice.local или 192.168.88.1']) }}
-                                    <details class="mt--3 small">
-                                        <summary>
-                                            Дополнительная информация
-                                        </summary>
-                                        ...
-                                    </details>
-                                </div>
-
-                                <div class="form-group">
-                                    <b>Порт</b>
-                                    <span class="small text-danger">*</span>
-                                    {{ Form::text('port', null, ['class' => 'form-control', 'required', 'placeholder' => 'Пример: 21']) }}
-                                    <details class="mt--3 small">
-                                        <summary>
-                                            Дополнительная информация
-                                        </summary>
-                                        ...
-                                    </details>
-                                </div>
-
-                                <div class="form-group">
-                                    <b>Логин</b>
-                                    <span class="small text-danger">*</span>
-                                    {{Form::text('login', null, ['class' => 'form-control', 'required', 'placeholder' => 'Пример: backup-user'])}}
-                                    <details class="mt--3 small">
-                                        <summary>
-                                            Дополнительная информация
-                                        </summary>
-                                        ...
-                                    </details>
-                                </div>
-
-                                <div class="form-group">
-                                    <b>Пароль</b>
-                                    {{Form::text('password', null, ['class' => 'form-control', 'placeholder' => 'Пример: 7{m5MUqpBDEmEXG'])}}
+                                    {{ Form::select('connector_id', $connectors, null, ['class' => 'form-control', 'required', 'placeholder' => 'Выберите коннектор...']) }}
                                     <details class="mt--3 small">
                                         <summary>
                                             Дополнительная информация
@@ -153,11 +117,11 @@
                                     <b>Interval</b>
                                     <span class="small text-danger">*</span>
                                     <div>
-                                        {{ Form::radio('interval', '1', false) }} every 1h
-                                        {{ Form::radio('interval', '3', false) }} every 3h
-                                        {{ Form::radio('interval', '6', false) }} every 6h
-                                        {{ Form::radio('interval', '12', false) }} every 12h
-                                        {{ Form::radio('interval', '24', false) }} every day
+                                        {{ Form::radio('interval', '1', null) }} every 1h
+                                        {{ Form::radio('interval', '3', null) }} every 3h
+                                        {{ Form::radio('interval', '6', null) }} every 6h
+                                        {{ Form::radio('interval', '12', null) }} every 12h
+                                        {{ Form::radio('interval', '24', null) }} every day
                                     </div>
                                     <details class="mt--3 small">
                                         <summary>
@@ -167,13 +131,27 @@
                                     </details>
                                 </div>
 
-                                <button type="submit" class="btn btn-xs bg-gradient-cyan">Добавить</button>
-                                {{ Form::close() }}
+                                <div class="form-group">
+                                    <b>Комментарий</b>
+                                    {{Form::textarea('comment', null, ['class' => 'form-control',  'rows' => 5, 'placeholder' => 'Комментарий...'])}}
+                                    <details class="mt--3 small">
+                                        <summary>
+                                            Дополнительная информация
+                                        </summary>
+                                        ...
+                                    </details>
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-xs bg-gradient-success">Обновить</button>
+                                    {{ Form::close() }}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
