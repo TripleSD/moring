c8f2e1a29229f4b9f9a5812b751bfd3b0ec11970
Anton Zheltyshev <az@maxrival.com>
2020-06-03 10:00:33 +0300

    Added new model.

---
+++ app/Models/BackupYandexConnectorsLogs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class BackupYandexConnectorsLogs.
+ */
+class BackupYandexConnectorsLogs extends Model
+{
+    protected $table = 'backup_yandex_connectors_logs';
+}
