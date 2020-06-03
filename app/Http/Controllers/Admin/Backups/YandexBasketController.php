2170b9f3c1df887ab8434d2526c21fade1d8c46d
Anton Zheltyshev <az@maxrival.com>
2020-06-03 14:46:54 +0300

    Added new function.

--- app/Http/Controllers/Admin/Backups/YandexBasketController.php
+++ app/Http/Controllers/Admin/Backups/YandexBasketController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin\Backups;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\Backups\YandexBasketRepository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\View\View;
+use App\Repositories\Backups\YandexConnectorRepository;
 
 /**
  * Class YandexBasketsController.
@@ -14,10 +16,12 @@ use Illuminate\View\View;
 class YandexBasketController extends Controller
 {
     private $yandexBasketRepository;
+    private $yandexConnectorRepository;
 
     public function __construct()
     {
         $this->yandexBasketRepository = new YandexBasketRepository();
+        $this->yandexConnectorRepository = new YandexConnectorRepository();
     }
 
     /**
@@ -29,5 +33,17 @@ class YandexBasketController extends Controller
         $baskets = $yandexBasketRepository->getList();
 
         return view('admin.backups.yandex.baskets.index', compact('baskets'));
+    }
+
+    /**
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function edit(Request $request)
+    {
+        $basket = $this->yandexBasketRepository->getBasket($request);
+        $connectors = $this->yandexConnectorRepository->getPluckList();
+
+        return view('admin.backups.yandex.baskets.edit', compact('basket', 'connectors'));
     }
 }
