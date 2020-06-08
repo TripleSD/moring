1cc7509bfacc7c9610f1357be1c33f7199a646fb
Anton Zheltyshev <az@maxrival.com>
2020-06-08 13:07:17 +0300

    Refactoring creating log event.

--- app/Repositories/System/SystemLogRepository.php
+++ app/Repositories/System/SystemLogRepository.php
@@ -7,34 +7,36 @@ use App\Repositories\Repository;
 
 /**
  * Class SystemLogRepository.
- * @package App\Repositories\System
  */
 class SystemLogRepository extends Repository
 {
     /**
+     * @param $functionName
      * @param $request
      * @return mixed
      */
-    public function createUserEvent($request)
+    public function createUserEvent($functionName, $request)
     {
         $logArray = [
             'service' => \Config::get('moring.service_system'),
             'debug_info' => $request->method(),
             'status' => $request->server('REDIRECT_STATUS'),
             'user_id' => \Auth::user()->id,
-            'route' => \Route::getCurrentRoute()->getActionName(),
+            'route' => \Route::getCurrentRoute()->getActionName() . PHP_EOL,
+            'callable_function' => $functionName
         ];
 
         return SystemLogs::create($logArray);
     }
 
     /**
+     * @param $functionName
      * @param null $service
      * @param null $status
      * @param null $debugInfo
      * @return mixed
      */
-    public function createServiceEvent($service = null, $status = null, $debugInfo = null)
+    public function createServiceEvent($functionName, $service = null, $status = null, $debugInfo = null)
     {
         if ($service === 'mysql') {
             $service = \Config::get('moring.service_mysql');
@@ -45,7 +47,8 @@ class SystemLogRepository extends Repository
             'debug_info' => $debugInfo,
             'status' => $status,
             'user_id' => \Auth::user()->id,
-            'route' => \Route::getCurrentRoute()->getActionName(),
+            'route' => \Route::getCurrentRoute()->getActionName() . PHP_EOL,
+            'callable_function' => $functionName
         ];
 
         return SystemLogs::create($logArray);
