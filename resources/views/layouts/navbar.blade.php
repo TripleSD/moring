a92d5156ed76a21598634fcba59c95ba5b8b0f17
Anton Zheltyshev <az@maxrival.com>
2020-06-08 17:16:45 +0300

    Added notifications/counts on header.

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -19,6 +19,28 @@
     </ul>
 
     <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link text-light" data-toggle="dropdown" href="#">
+                <i class="far fa-bell"></i>
+                <span class="badge navbar-badge badge-{{ ($totalCount > 0) ? 'warning' : 'success' }}">
+                    {{ $totalCount }}
+                </span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">{{ $totalCount }} Notifications</span>
+                <div class="dropdown-divider"></div>
+                <a href="{{ route('backups.yandex.connectors.index') }}" class="dropdown-item">
+                    <i class="fas fa-exclamation-triangle"></i>
+                    <span class="badge badge-{{ ($yandexConnectorsLogsCount >0 ? 'danger' : 'success') }}">
+                        {{ $yandexConnectorsLogsCount }}
+                    </span>
+                    Connectors alerts
+                    <span class="float-right text-muted text-xs">{{ $yandexConnectorsLogsLastEvent }}</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+            </div>
+        </li>
         <li class="nav-item p-1">
             @if(App::getLocale() === 'ru')
                 <a href="{{ route('setLocale',['en']) }}"><img src="/img/theme/english.png" align="en"></a>
