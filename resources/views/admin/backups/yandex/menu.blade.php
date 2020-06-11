eb1a38817a18af0cee4d1bd024093c1d4c727d38
Anton Zheltyshev <az@maxrival.com>
2020-06-11 18:53:59 +0300

    Changed titles button's from lang file.

--- resources/views/admin/backups/yandex/menu.blade.php
+++ resources/views/admin/backups/yandex/menu.blade.php
@@ -1,16 +1,17 @@
 <div class="btn-group">
     <a class="btn btn-xs {{ (request()->route()->named('backups.yandex.connectors.index')) ? 'btn-danger' : 'btn-dark' }}"
        href="{{ route('backups.yandex.connectors.index') }}"
-       title="Просмотр устройства">
+       title="@lang('messages.backups.yandex.buttons.title.connectors')">
         @lang('messages.backups.yandex.breadcrumbs.small.connectors')
     </a>
     <a class="btn btn-xs {{ (request()->route()->named('backups.yandex.tasks.index')) ? 'btn-danger' : 'btn-dark' }}"
-       href="{{ route('backups.yandex.tasks.index') }}" title="Просмотр устройства">
+       href="{{ route('backups.yandex.tasks.index') }}"
+       title="@lang('messages.backups.yandex.buttons.title.tasks')">
         @lang('messages.backups.yandex.breadcrumbs.small.tasks')
     </a>
     <a class="btn btn-xs {{ (request()->route()->named('backups.yandex.buckets.index')) ? 'btn-danger' : 'btn-dark' }}"
        href="{{ route('backups.yandex.buckets.index') }}"
-       title="Редактирование устройства">
+       title="@lang('messages.backups.yandex.buttons.title.buckets')">
         @lang('messages.backups.yandex.breadcrumbs.small.buckets')
     </a>
 </div>
