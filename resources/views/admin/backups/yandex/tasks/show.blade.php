686a7a4bc45c6bdcf2c5f03a3b9744321a949999
Anton Zheltyshev <az@maxrival.com>
2020-06-22 12:40:05 +0300

    Updating template.

--- resources/views/admin/backups/yandex/tasks/show.blade.php
+++ resources/views/admin/backups/yandex/tasks/show.blade.php
@@ -74,138 +74,158 @@
 
             <div class="row">
                 <div class="col-sm-6">
-                    <div class="card card-info">
+                    <div class="card">
                         <div class="card-header">
                               <span class="text-muted text-sm">
                                   @lang('messages.backups.yandex.tasks.titles.summary_information')
                               </span>
                         </div>
                         <div class="card-body">
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
-                                    <span class="text-bold">
-                                        @lang('messages.backups.yandex.fields.description'):
-                                    </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->description }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                            <ul class="nav nav-pills flex-column">
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
+                                            <span class="text-bold">
+                                                @lang('messages.backups.yandex.fields.description'):
+                                            </span>
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->description }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small mt--3">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                         @lang('messages.backups.yandex.fields.status'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    @if($task->enabled === 1)
-                                        <div class="badge badge-success">
-                                            @lang('messages.backups.yandex.fields.enabled')
                                         </div>
-                                    @else
-                                        <div class="small badge badge-secondary">
-                                            @lang('messages.backups.yandex.fields.disabled')
+                                        <div class="col-lg-8 col-7">
+                                            @if($task->enabled === 1)
+                                                <div class="badge badge-success">
+                                                    @lang('messages.backups.yandex.fields.enabled')
+                                                </div>
+                                            @else
+                                                <div class="small badge badge-secondary">
+                                                    @lang('messages.backups.yandex.fields.disabled')
+                                                </div>
+                                            @endif
                                         </div>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                         @lang('messages.backups.yandex.fields.connector'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->connector->description }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
-                                    <span class="text-bold">
-                                        @lang('messages.backups.yandex.fields.folder'):
-                                    </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->folder }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
-                                    <span class="text-bold">
-                                        @lang('messages.backups.yandex.fields.pre_suffix'):
-                                    </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->pre }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->connector->description }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
+                                            <span class="text-bold">
+                                                @lang('messages.backups.yandex.fields.folder'):
+                                            </span>
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->folder }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
+                                            <span class="text-bold">
+                                                @lang('messages.backups.yandex.fields.pre_suffix'):
+                                            </span>
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->pre }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                         @lang('messages.backups.yandex.fields.post_suffix'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->post }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->post }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                         @lang('messages.backups.yandex.fields.file'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->filename }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->filename }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                        @lang('messages.backups.yandex.fields.interval'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->interval }}
-                                </div>
-                            </div>
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->interval }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                        @lang('messages.backups.yandex.fields.last_check'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->updated_at }}
-                                </div>
-                            </div>
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->updated_at }}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="small">
 
-                            <hr/>
-
-                            <div class="row small">
-                                <div class="col-lg-4 col-5">
+                                    <div class="row">
+                                        <div class="col-lg-4 col-5">
                                     <span class="text-bold">
                                         @lang('messages.backups.yandex.fields.comment'):
                                     </span>
-                                </div>
-                                <div class="col-lg-8 col-7">
-                                    {{ $task->comment }}
-                                </div>
-                            </div>
+                                        </div>
+                                        <div class="col-lg-8 col-7">
+                                            {{ $task->comment }}
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
 
                 <div class="col-sm-6">
                     @if($task->active_logs->count() !== 0)
-                        <div class="card card-warning">
+                        <div class="card">
                             <div class="card-header">
                                 <span class="text-muted text-sm">
                                     @lang('messages.backups.yandex.tasks.titles.errors')
                                 </span>
                                 <span class="badge badge-{{ ($task->active_logs->count() > 0) ? 'danger' : 'success'}}">
-                                {{ $task->active_logs->count() }}
-                            </span>
+                                    {{ $task->active_logs->count() }}
+                                </span>
                             </div>
                             @if($task->active_logs !== null)
                                 <div class="card-body table-responsive p-0">
@@ -258,7 +278,7 @@
                             @endif
                         </div>
                     @else
-                        <div class="card card-gray">
+                        <div class="card">
                             <div class="card-header">
                                 <dt>@lang('messages.network.device.notifications_and_errors')</dt>
                             </div>
